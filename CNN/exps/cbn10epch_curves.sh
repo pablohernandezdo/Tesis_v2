413732e071993491f63b42e63f4b57b320bd0b1c
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-07-26 21:48:28 -0400

    Change experiments and loss

--- CNN/exps/cbn10epch_curves.sh
+++ CNN/exps/cbn10epch_curves.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p ../logs/CBN_v2/train
+mkdir -p ../logs/CBN/train_val
 mkdir -p ../models
 
 trn="Train_data.hdf5"
@@ -9,5 +9,5 @@ tst="Test_data.hdf5"
 # Train ClassConv_BN for 1 epoch
 echo "Training CBN model on $trn and $tst datasets"
 python ../train_curves.py --train_path $trn --test_path $tst \
-              --classifier CBN_v2 --model_name CBN_v2_10epch \
-              --n_epochs 10 --batch_size 32 --lr 1e-6  > ../logs/CBN_v2/train/CBN_v2_10epch_curves.txt
+              --classifier CBN_val --model_name CBN_val_10epch \
+              --n_epochs 10 --batch_size 32 --lr 1e-6  > ../logs/CBN/train_val/CBN_val_10epch_curves.txt
