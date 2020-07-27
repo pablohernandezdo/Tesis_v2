be30a31057f719b9992a833aff38794ceba99181
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-07-27 19:54:24 -0400

    Update exp

--- CNN/exps/cbn1epch.sh
+++ CNN/exps/cbn1epch.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 mkdir -p ../logs/CBN/train
-mkdir -p ../models
+mkdir -p ../logs/CBN/eval
+mkdir -p ../models/
 
 trn="Train_data.hdf5"
 tst="Test_data.hdf5"
 
-# Train ClassConv_BN for 1 epoch
+# Train CBN for 1 epoch
 echo "Training CBN model on $trn and $tst datasets"
 python ../train.py --train_path $trn --test_path $tst \
               --classifier CBN --model_name CBN_1epch \
               --n_epochs 1 --batch_size 32 --lr 1e-6  > ../logs/CBN/train/CBN_1epch.txt
+
+echo "Evaluating CBN model on $trn and $tst datasets"
+python ../eval.py --train_path $trn --test_path $tst \
+              --classifier CBN --model_name CBN_1epch > ../logs/CBN/eval/CBN_1epch.txt
