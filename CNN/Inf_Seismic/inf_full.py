6aa2a7151f689c6423228f214c11d2ba86299b39
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-07-29 19:30:07 -0400

    Print datasets len

--- CNN/Inf_Seismic/inf_full.py
+++ CNN/Inf_Seismic/inf_full.py
@@ -64,26 +64,28 @@ def main():
     traces = np.asarray(traces)
     traces = traces[:66]
 
+    print(f'Seismic Francia: {len(traces)}')
+
     # For every trace in the file
-    for trace in traces:
-        if np.max(np.abs(trace)):
-            # Normalize
-            trace = trace / np.max(np.abs(trace))
-
-            # Numpy to Torch
-            trace = torch.from_numpy(trace).to(device).unsqueeze(0)
-
-            # Prediction
-            out_trace = net(trace.float())
-            pred_trace = torch.round(out_trace.data).item()
-
-            # Count traces
-            total_seismic += 1
-
-            if pred_trace:
-                tp += 1
-            else:
-                fn += 1
+    # for trace in traces:
+    #     if np.max(np.abs(trace)):
+    #         # Normalize
+    #         trace = trace / np.max(np.abs(trace))
+    #
+    #         # Numpy to Torch
+    #         trace = torch.from_numpy(trace).to(device).unsqueeze(0)
+    #
+    #         # Prediction
+    #         out_trace = net(trace.float())
+    #         pred_trace = torch.round(out_trace.data).item()
+    #
+    #         # Count traces
+    #         total_seismic += 1
+    #
+    #         if pred_trace:
+    #             tp += 1
+    #         else:
+    #             fn += 1
 
     # # Load Nevada data file 721
     f = '../../Data/Nevada/PoroTomo_iDAS16043_160321073721.sgy'
@@ -149,33 +151,35 @@ def main():
     tr3 = traces[4800:8650]
     traces = np.vstack((tr1, tr2, tr3))
 
+    print(f'Seismic Nevada: {len(traces)}')
+
     # npad = 1500
 
-    # For every trace in the file
-    for trace in traces:
-        # Resample
-        trace = signal.resample(trace, 6000)
-
-        # Zero pad
-        # trace = np.pad(trace, (npad, npad), mode='constant')
-
-        # Normalize
-        trace = trace / np.max(np.abs(trace))
-
-        # Numpy to Torch
-        trace = torch.from_numpy(trace).to(device).unsqueeze(0)
-
-        # Prediction
-        out_trace = net(trace.float())
-        pred_trace = torch.round(out_trace.data).item()
-
-        # Count traces
-        total_seismic += 1
-
-        if pred_trace:
-            tp += 1
-        else:
-            fn += 1
+    # # For every trace in the file
+    # for trace in traces:
+    #     # Resample
+    #     trace = signal.resample(trace, 6000)
+    #
+    #     # Zero pad
+    #     # trace = np.pad(trace, (npad, npad), mode='constant')
+    #
+    #     # Normalize
+    #     trace = trace / np.max(np.abs(trace))
+    #
+    #     # Numpy to Torch
+    #     trace = torch.from_numpy(trace).to(device).unsqueeze(0)
+    #
+    #     # Prediction
+    #     out_trace = net(trace.float())
+    #     pred_trace = torch.round(out_trace.data).item()
+    #
+    #     # Count traces
+    #     total_seismic += 1
+    #
+    #     if pred_trace:
+    #         tp += 1
+    #     else:
+    #         fn += 1
 
     # # Load Nevada dataset file 747
     # f = '../Data/Nevada/PoroTomo_iDAS025_160321073747.sgy'
@@ -272,109 +276,109 @@ def main():
     #     else:
     #         fn += 1
 
-    # Predict average 5km of measurements
-    avg_data = np.mean(data[3500:4001, :], 0)
-
-    # Resample
-    avg_data = signal.resample(avg_data, 6000)
-
-    # Normalize
-    avg_data = avg_data / np.max(np.abs(avg_data))
-
-    # Numpy to Torch
-    avg_data = torch.from_numpy(avg_data).to(device).unsqueeze(0)
-
-    # Prediction
-    output = net(avg_data.float())
-
-    predicted = torch.round(output.data).item()
-
-    total_nseismic += 1
-
-    if predicted:
-        tp += 1
-    else:
-        fn += 1
+    # # Predict average 5km of measurements
+    # avg_data = np.mean(data[3500:4001, :], 0)
+    #
+    # # Resample
+    # avg_data = signal.resample(avg_data, 6000)
+    #
+    # # Normalize
+    # avg_data = avg_data / np.max(np.abs(avg_data))
+    #
+    # # Numpy to Torch
+    # avg_data = torch.from_numpy(avg_data).to(device).unsqueeze(0)
+    #
+    # # Prediction
+    # output = net(avg_data.float())
+    #
+    # predicted = torch.round(output.data).item()
+    #
+    # total_nseismic += 1
+    #
+    # if predicted:
+    #     tp += 1
+    # else:
+    #     fn += 1
 
     # Reykjanes telesismo fibra optica
-    file_fo = '../../Data/Reykjanes/Jousset_et_al_2018_003_Figure3_fo.ascii'
-
-    # Dict for header and data
-    data_fo = {
-        'head': '',
-        'strain': []
-    }
-
-    # Read fo file and save content to data_fo
-    with open(file_fo, 'r') as f:
-        for idx, line in enumerate(f):
-            if idx == 0:
-                data_fo['head'] = line.strip()
-            else:
-                val = line.strip()
-                data_fo['strain'].append(float(val))
-
-    # Resample
-    data_fo['strain'] = signal.resample(data_fo['strain'], 6000)
-
-    # Normalize
-    data_fo['strain'] = data_fo['strain'] / np.max(np.abs(data_fo['strain']))
-
-    # Numpy to Torch
-    data_fo['strain'] = torch.from_numpy(data_fo['strain']).to(device).unsqueeze(0)
-
-    # Prediction
-    out = net(data_fo['strain'].float())
-    predicted = torch.round(out.data).item()
-
-    if predicted:
-        tp += 1
-    else:
-        fn += 1
-
-    # Registro de sismo local con DAS
-    file = '../../Data/Reykjanes/Jousset_et_al_2018_003_Figure5b.ascii'
-    n_trazas = 2551
-
-    data = {
-        'head': '',
-        'strain': np.empty((1, n_trazas))
-    }
-
-    with open(file, 'r') as f:
-        for idx, line in enumerate(f):
-            if idx == 0:
-                data['head'] = line.strip()
-
-            else:
-                row = np.asarray(list(map(float, re.sub(' +', ' ', line).strip().split(' '))))
-                data['strain'] = np.concatenate((data['strain'], np.expand_dims(row, 0)))
-
-    data['strain'] = data['strain'][1:]
-    traces = data['strain'].transpose()
-
-    # For every trace in the file
-    for trace in traces:
-        # Resample
-        trace = signal.resample(trace, 6000)
-
-        # Normalize
-        trace = trace / np.max(np.abs(trace))
-
-        # Numpy to Torch
-        trace = torch.from_numpy(trace).to(device).unsqueeze(0)
-
-        # Prediction
-        out_trace = net(trace.float())
-        pred_trace = torch.round(out_trace.data).item()
-
-        # Count traces
-        total_seismic += 1
-
-        if pred_trace:
-            tp += 1
-        else:
-            fn += 1
+    # file_fo = '../../Data/Reykjanes/Jousset_et_al_2018_003_Figure3_fo.ascii'
+    #
+    # # Dict for header and data
+    # data_fo = {
+    #     'head': '',
+    #     'strain': []
+    # }
+    #
+    # # Read fo file and save content to data_fo
+    # with open(file_fo, 'r') as f:
+    #     for idx, line in enumerate(f):
+    #         if idx == 0:
+    #             data_fo['head'] = line.strip()
+    #         else:
+    #             val = line.strip()
+    #             data_fo['strain'].append(float(val))
+    #
+    # # Resample
+    # data_fo['strain'] = signal.resample(data_fo['strain'], 6000)
+    #
+    # # Normalize
+    # data_fo['strain'] = data_fo['strain'] / np.max(np.abs(data_fo['strain']))
+    #
+    # # Numpy to Torch
+    # data_fo['strain'] = torch.from_numpy(data_fo['strain']).to(device).unsqueeze(0)
+    #
+    # # Prediction
+    # out = net(data_fo['strain'].float())
+    # predicted = torch.round(out.data).item()
+    #
+    # if predicted:
+    #     tp += 1
+    # else:
+    #     fn += 1
+    #
+    # # Registro de sismo local con DAS
+    # file = '../../Data/Reykjanes/Jousset_et_al_2018_003_Figure5b.ascii'
+    # n_trazas = 2551
+    #
+    # data = {
+    #     'head': '',
+    #     'strain': np.empty((1, n_trazas))
+    # }
+    #
+    # with open(file, 'r') as f:
+    #     for idx, line in enumerate(f):
+    #         if idx == 0:
+    #             data['head'] = line.strip()
+    #
+    #         else:
+    #             row = np.asarray(list(map(float, re.sub(' +', ' ', line).strip().split(' '))))
+    #             data['strain'] = np.concatenate((data['strain'], np.expand_dims(row, 0)))
+    #
+    # data['strain'] = data['strain'][1:]
+    # traces = data['strain'].transpose()
+    #
+    # # For every trace in the file
+    # for trace in traces:
+    #     # Resample
+    #     trace = signal.resample(trace, 6000)
+    #
+    #     # Normalize
+    #     trace = trace / np.max(np.abs(trace))
+    #
+    #     # Numpy to Torch
+    #     trace = torch.from_numpy(trace).to(device).unsqueeze(0)
+    #
+    #     # Prediction
+    #     out_trace = net(trace.float())
+    #     pred_trace = torch.round(out_trace.data).item()
+    #
+    #     # Count traces
+    #     total_seismic += 1
+    #
+    #     if pred_trace:
+    #         tp += 1
+    #     else:
+    #         fn += 1
 
     # # Load California dataset file
     f = scipy.io.loadmat('../../Data/California/FSE-06_480SecP_SingDec_StepTest (1).mat')
@@ -383,7 +387,8 @@ def main():
     traces = f['singdecmatrix']
     traces = traces.transpose()
 
-    # For every trace in the file
+    cali_traces = 0
+
     for tr in traces:
         # Resample
         tr = signal.resample(tr, 41228)
@@ -391,60 +396,72 @@ def main():
         tr = np.reshape(tr, (-1, 6000))
 
         for trace in tr:
-            # Normalize
-            trace = trace / np.max(np.abs(trace))
+            cali_traces += 1
 
-            # Numpy to Torch
-            trace = torch.from_numpy(trace).to(device).unsqueeze(0)
+    print(f'Cali traces: {cali_traces}')
 
-            # Prediction
-            out_trace = net(trace.float())
-            pred_trace = torch.round(out_trace.data).item()
-
-            # Count traces
-            total_nseismic += 1
-
-            if pred_trace:
-                fp += 1
-            else:
-                tn += 1
+    # # For every trace in the file
+    # for tr in traces:
+    #     # Resample
+    #     tr = signal.resample(tr, 41228)
+    #     tr = tr[:36000]
+    #     tr = np.reshape(tr, (-1, 6000))
+    #
+    #     for trace in tr:
+    #         # Normalize
+    #         trace = trace / np.max(np.abs(trace))
+    #
+    #         # Numpy to Torch
+    #         trace = torch.from_numpy(trace).to(device).unsqueeze(0)
+    #
+    #         # Prediction
+    #         out_trace = net(trace.float())
+    #         pred_trace = torch.round(out_trace.data).item()
+    #
+    #         # Count traces
+    #         total_nseismic += 1
+    #
+    #         if pred_trace:
+    #             fp += 1
+    #         else:
+    #             tn += 1
 
     # # File name
-    file = '../../Data/Hydraulic/CSULB500Pa600secP_141210183813.mat'
-
-    # Read file data
-    with h5py.File(file, 'r') as f:
-        traces = f['data'][()]
-
-    # Reshape 6000 first samples
-    traces = traces[:, :6000]
-
-    # For every trace in the file
-    for tr in traces:
-        # Resample
-        tr = signal.resample(tr, 120000)
-
-        # Reshape
-        tr = np.reshape(tr, (-1, 6000))
-
-        for trace in tr:
-            # Normalize
-            trace = trace / np.max(np.abs(trace))
-
-            # Numpy to Torch
-            trace = torch.from_numpy(trace).to(device).unsqueeze(0)
-
-            # Prediction
-            out_trace = net(trace.float())
-            pred_trace = torch.round(out_trace.data).item()
-
-            # Count traces
-            total_nseismic += 1
-
-            if pred_trace:
-                fp += 1
-            else:
-                tn += 1
+    # file = '../../Data/Hydraulic/CSULB500Pa600secP_141210183813.mat'
+    #
+    # # Read file data
+    # with h5py.File(file, 'r') as f:
+    #     traces = f['data'][()]
+    #
+    # # Reshape 6000 first samples
+    # traces = traces[:, :6000]
+    #
+    # # For every trace in the file
+    # for tr in traces:
+    #     # Resample
+    #     tr = signal.resample(tr, 120000)
+    #
+    #     # Reshape
+    #     tr = np.reshape(tr, (-1, 6000))
+    #
+    #     for trace in tr:
+    #         # Normalize
+    #         trace = trace / np.max(np.abs(trace))
+    #
+    #         # Numpy to Torch
+    #         trace = torch.from_numpy(trace).to(device).unsqueeze(0)
+    #
+    #         # Prediction
+    #         out_trace = net(trace.float())
+    #         pred_trace = torch.round(out_trace.data).item()
+    #
+    #         # Count traces
+    #         total_nseismic += 1
+    #
+    #         if pred_trace:
+    #             fp += 1
+    #         else:
+    #             tn += 1
 
     # file = '../../Data/Hydraulic/CSULB500Pa10secP_141210174309.mat'
     #
@@ -737,103 +754,121 @@ def main():
     #         fp += 1
     #     else:
     #         tn += 1
-    #
-    # # Init rng
-    # rng = default_rng()
-    #
-    # # Noise
-    # ns = rng.normal(0, 1, 6000)
-    #
-    # # Sine waves
-    #
-    # # Number of sample points
-    # N = 6000
-    #
-    # # sampling frequency
-    # fs = 100
-    #
-    # # Time axis
-    # t = np.linspace(0.0, N / fs, N)
-    #
-    # # Number of frequency interval steps
-    # n = 100
-    #
-    # # Frequency spans
-    # fr1 = np.linspace(1, 50, n)
-    # fr2 = np.linspace(0.01, 1, n)
-    #
-    # # Prealocate
-    # wvs1 = []
-    # wvs2 = []
-    # wvs3 = []
-    #
-    # for f1, f2 in zip(fr1, fr2):
-    #     sig1 = np.sin(f1 * 2.0 * np.pi * t)
-    #     sig2 = np.sin(f2 * 2.0 * np.pi * t)
-    #     wvs1.append(sig1)
-    #     wvs2.append(sig2)
-    #     wvs3.append(sig1 + sig2)
-    #
-    # wvs1 = np.array(wvs1)
-    # wvs2 = np.array(wvs2)
-    # wvs3 = np.array(wvs3)
-    #
-    # wvs1_ns = wvs1 + 0.5 * rng.normal(0, 1, wvs1.shape)
-    # wvs2_ns = wvs2 + 0.5 * rng.normal(0, 1, wvs2.shape)
-    # wvs3_ns = wvs3 + 0.5 * rng.normal(0, 1, wvs3.shape)
-    #
-    # # PADDED SINES
-    #
-    # # Number of intermediate sample points
-    # ni = [1000, 2000, 4000, 5000]
-    #
-    # # Number of points to zero-pad
-    # pad = [(N-n) // 2 for n in ni]
-    #
-    # # Time axis for smaller waves
-    # lts = [np.linspace(0.0, nis / fs, nis) for nis in ni]
-    #
-    # # All frequencies list
-    # all_fr = []
-    #
-    # # Calculate max period for smaller waves
-    # max_periods = [n_points / fs for n_points in ni]
-    #
-    # # Calculate frequencies for smaller waves
-    # for per in max_periods:
-    #     freqs = []
-    #     for i in range(1, int(per)+1):
-    #         if per % i == 0:
-    #             freqs.append(1/i)
-    #     all_fr.append(freqs)
-    #
-    # # Preallocate waves
-    # wvs_pad = []
-    #
-    # # Generate waves and zero pad
-    # for idx, fr_ls in enumerate(all_fr):
-    #     for fr in fr_ls:
-    #         wv = np.sin(fr * 2.0 * np.pi * lts[idx])
-    #         wv = np.pad(wv, (pad[idx], pad[idx]), 'constant')
-    #         wvs_pad.append(wv)
-    #
-    # # Wavelets
-    #
-    # # Preallocate wavelets
-    # lets = []
-    #
-    # # Discrete wavelet families
-    # discrete_families = ['db', 'sym', 'coif', 'bior', 'rbio']
-    #
-    # # Obtain wavelet waveforms, resample and append
-    # for fam in discrete_families:
-    #     for wavelet in pywt.wavelist(fam):
-    #         wv = pywt.Wavelet(wavelet)
-    #         if wv.orthogonal:
-    #             [_, psi, _] = pywt.Wavelet(wavelet).wavefun(level=5)
-    #             psi = signal.resample(psi, 6000)
-    #             lets.append(psi)
-    #
+
+    # Señales de prueba
+    # Init rng
+    rng = default_rng()
+
+    sig_prueba = 0
+
+    # Noise
+    ns = rng.normal(0, 1, 6000)
+
+    sig_prueba += 1
+
+    # Sine waves
+
+    # Number of sample points
+    N = 6000
+
+    # sampling frequency
+    fs = 100
+
+    # Time axis
+    t = np.linspace(0.0, N / fs, N)
+
+    # Number of frequency interval steps
+    n = 100
+
+    # Frequency spans
+    fr1 = np.linspace(1, 50, n)
+    fr2 = np.linspace(0.01, 1, n)
+
+    # Prealocate
+    wvs1 = []
+    wvs2 = []
+    wvs3 = []
+
+    for f1, f2 in zip(fr1, fr2):
+        sig1 = np.sin(f1 * 2.0 * np.pi * t)
+        sig2 = np.sin(f2 * 2.0 * np.pi * t)
+        wvs1.append(sig1)
+        wvs2.append(sig2)
+        wvs3.append(sig1 + sig2)
+
+    wvs1 = np.array(wvs1)
+    wvs2 = np.array(wvs2)
+    wvs3 = np.array(wvs3)
+
+    wvs1_ns = wvs1 + 0.5 * rng.normal(0, 1, wvs1.shape)
+    wvs2_ns = wvs2 + 0.5 * rng.normal(0, 1, wvs2.shape)
+    wvs3_ns = wvs3 + 0.5 * rng.normal(0, 1, wvs3.shape)
+
+    sig_prueba += len(wvs1)
+    sig_prueba += len(wvs2)
+    sig_prueba += len(wvs3)
+    sig_prueba += len(wvs1_ns)
+    sig_prueba += len(wvs2_ns)
+    sig_prueba += len(wvs3_ns)
+
+    # PADDED SINES
+
+    # Number of intermediate sample points
+    ni = [1000, 2000, 4000, 5000]
+
+    # Number of points to zero-pad
+    pad = [(N-n) // 2 for n in ni]
+
+    # Time axis for smaller waves
+    lts = [np.linspace(0.0, nis / fs, nis) for nis in ni]
+
+    # All frequencies list
+    all_fr = []
+
+    # Calculate max period for smaller waves
+    max_periods = [n_points / fs for n_points in ni]
+
+    # Calculate frequencies for smaller waves
+    for per in max_periods:
+        freqs = []
+        for i in range(1, int(per)+1):
+            if per % i == 0:
+                freqs.append(1/i)
+        all_fr.append(freqs)
+
+    # Preallocate waves
+    wvs_pad = []
+
+    # Generate waves and zero pad
+    for idx, fr_ls in enumerate(all_fr):
+        for fr in fr_ls:
+            wv = np.sin(fr * 2.0 * np.pi * lts[idx])
+            wv = np.pad(wv, (pad[idx], pad[idx]), 'constant')
+            wvs_pad.append(wv)
+
+    sig_prueba += len(wvs_pad)
+
+    # Wavelets
+
+    # Preallocate wavelets
+    lets = []
+
+    # Discrete wavelet families
+    discrete_families = ['db', 'sym', 'coif', 'bior', 'rbio']
+
+    # Obtain wavelet waveforms, resample and append
+    for fam in discrete_families:
+        for wavelet in pywt.wavelist(fam):
+            wv = pywt.Wavelet(wavelet)
+            if wv.orthogonal:
+                [_, psi, _] = pywt.Wavelet(wavelet).wavefun(level=5)
+                psi = signal.resample(psi, 6000)
+                lets.append(psi)
+
+    sig_prueba += len(lets)
+
+    print(sig_prueba)
+    
     # # Normalize
     # ns_norm = ns / np.max(np.abs(ns))
     #
@@ -1007,23 +1042,23 @@ def main():
     #         fp += 1
     #     else:
     #         tn += 1
-
-    # Evaluation metrics
-    precision = tp / (tp + fp)
-    recall = tp / (tp + fn)
-    fscore = 2 * (precision * recall) / (precision + recall)
-
-    # Results
-    print(f'Total seismic traces: {total_seismic}\n'
-          f'Total non seismic traces: {total_nseismic}\n'
-          f'True positives: {tp}\n'
-          f'False positives: {fp}\n'
-          f'True negatives: {tn}\n'
-          f'False negatives: {fn}\n\n'
-          f'Evaluation metrics:\n\n'          
-          f'Precision: {precision:5.3f}\n'
-          f'Recall: {recall:5.3f}\n'
-          f'F-score: {fscore:5.3f}\n')
+    #
+    # # Evaluation metrics
+    # precision = tp / (tp + fp)
+    # recall = tp / (tp + fn)
+    # fscore = 2 * (precision * recall) / (precision + recall)
+    #
+    # # Results
+    # print(f'Total seismic traces: {total_seismic}\n'
+    #       f'Total non seismic traces: {total_nseismic}\n'
+    #       f'True positives: {tp}\n'
+    #       f'False positives: {fp}\n'
+    #       f'True negatives: {tn}\n'
+    #       f'False negatives: {fn}\n\n'
+    #       f'Evaluation metrics:\n\n'
+    #       f'Precision: {precision:5.3f}\n'
+    #       f'Recall: {recall:5.3f}\n'
+    #       f'F-score: {fscore:5.3f}\n')
 
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
