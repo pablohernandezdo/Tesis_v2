4f0cfd0530c559a0283cc40bf1046b4e7302be77
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-07-29 12:33:44 -0400

    Utah anim

--- Read-NSeismic/read_utah.py
+++ Read-NSeismic/read_utah.py
@@ -16,7 +16,7 @@ from pathlib import Path
 def main():
     # Create images and animations folder
 
-    Path("Imgs").mkdir(exist_ok=True)
+    Path("Imgs/Utah").mkdir(parents=True, exist_ok=True)
 
     # Carga traza STEAD
 
@@ -36,23 +36,23 @@ def main():
         traces = segyio.tools.collect(segy.trace[:])
         fs = segy.header[0][117]
 
-    # Number of traces to plot
-    n = 4
-
-    # Traces to plot
-    trtp = []
-
-    # Init rng
-    rng = default_rng()
-
-    # Traces to plot numbers
-    trtp_ids = rng.choice(len(traces), size=n, replace=False)
-    trtp_ids.sort()
-
-    # Retrieve selected traces
-    for idx, trace in enumerate(traces):
-        if idx in trtp_ids:
-            trtp.append(trace)
+    # # Number of traces to plot
+    # n = 4
+    #
+    # # Traces to plot
+    # trtp = []
+    #
+    # # Init rng
+    # rng = default_rng()
+    #
+    # # Traces to plot numbers
+    # trtp_ids = rng.choice(len(traces), size=n, replace=False)
+    # trtp_ids.sort()
+    #
+    # # Retrieve selected traces
+    # for idx, trace in enumerate(traces):
+    #     if idx in trtp_ids:
+    #         trtp.append(trace)
 
     # Data len
     N = traces.shape[1]
@@ -63,61 +63,61 @@ def main():
     # Frequency axis for FFT plot
     xf = np.linspace(-fs / 2.0, fs / 2.0 - 1 / fs, N)
 
-    # Figure to plot
-    plt.figure()
+    # # Figure to plot
+    # plt.figure()
 
-    # For trace in traces to print
-    for idx, trace in enumerate(trtp):
-        yf = sfft.fftshift(sfft.fft(trace))
+    # # For trace in traces to print
+    # for idx, trace in enumerate(trtp):
+    #     yf = sfft.fftshift(sfft.fft(trace))
+    #
+    #     plt.clf()
+    #     plt.subplot(211)
+    #     plt.plot(t_ax, trace)
+    #     plt.title(f'Traza Utah y espectro #{trtp_ids[idx]}')
+    #     plt.xlabel('Tiempo [s]')
+    #     plt.ylabel('Amplitud [-]')
+    #     plt.grid(True)
+    #
+    #     plt.subplot(212)
+    #     plt.plot(xf, np.abs(yf) / np.max(np.abs(yf)))
+    #     plt.xlabel('Frecuencia [Hz]')
+    #     plt.ylabel('Amplitud [-]')
+    #     plt.grid(True)
+    #     plt.tight_layout()
+    #     plt.savefig(f'Imgs/Utah/Utah_{trtp_ids[idx]}')
 
-        plt.clf()
-        plt.subplot(211)
-        plt.plot(t_ax, trace)
-        plt.title(f'Traza Utah y espectro #{trtp_ids[idx]}')
+    # Create animation of whole data
+    fig_tr = plt.figure()
+    ims_tr = []
+
+    for trace in traces:
+        im_tr = plt.plot(t_ax, trace)
+        plt.title('Trazas dataset DAS no sísmico Utah')
+        plt.ylabel('Amplitud normalizada[-]')
         plt.xlabel('Tiempo [s]')
-        plt.ylabel('Amplitud [-]')
-        plt.grid(True)
-
-        plt.subplot(212)
-        plt.plot(xf, np.abs(yf) / np.max(np.abs(yf)))
-        plt.xlabel('Frecuencia [Hz]')
-        plt.ylabel('Amplitud [-]')
         plt.grid(True)
         plt.tight_layout()
-        plt.savefig(f'Imgs/Utah_{trtp_ids[idx]}')
+        ims_tr.append(im_tr)
 
-    # # Create animation of whole data
-    # fig_tr = plt.figure()
-    # ims_tr = []
-    #
-    # for trace in traces:
-    #     im_tr = plt.plot(t_ax, trace)
-    #     plt.title('Trazas dataset DAS no sísmico Utah')
-    #     plt.ylabel('Amplitud normalizada[-]')
-    #     plt.xlabel('Tiempo [s]')
-    #     plt.grid(True)
-    #     plt.tight_layout()
-    #     ims_tr.append(im_tr)
-    #
-    # ani_tr = animation.ArtistAnimation(fig_tr, ims_tr, interval=50, blit=True, repeat=False)
-    # ani_tr.save('Animations/Utah_traces.mp4')
-    #
-    # # Create animation of whole data spectrums
-    # fig_sp = plt.figure()
-    # ims_sp = []
-    #
-    # for trace in traces:
-    #     yf = sfft.fftshift(sfft.fft(trace))
-    #     im_sp = plt.plot(xf, np.abs(yf) / np.max(np.abs(yf)))
-    #     plt.title('Espectro trazas dataset DAS no sísmico Utah')
-    #     plt.ylabel('Amplitud [-]')
-    #     plt.xlabel('Frecuencia [Hz]')
-    #     plt.grid(True)
-    #     plt.tight_layout()
-    #     ims_sp.append(im_sp)
-    #
-    # ani_sp = animation.ArtistAnimation(fig_sp, ims_sp, interval=50, blit=True, repeat=False)
-    # ani_sp.save('Animations/Utah_spectrums.mp4')
+    ani_tr = animation.ArtistAnimation(fig_tr, ims_tr, interval=50, blit=True, repeat=False)
+    ani_tr.save('Animations/Utah/Utah_traces.mp4')
+
+    # Create animation of whole data spectrums
+    fig_sp = plt.figure()
+    ims_sp = []
+
+    for trace in traces:
+        yf = sfft.fftshift(sfft.fft(trace))
+        im_sp = plt.plot(xf, np.abs(yf) / np.max(np.abs(yf)))
+        plt.title('Espectro trazas dataset DAS no sísmico Utah')
+        plt.ylabel('Amplitud [-]')
+        plt.xlabel('Frecuencia [Hz]')
+        plt.grid(True)
+        plt.tight_layout()
+        ims_sp.append(im_sp)
+
+    ani_sp = animation.ArtistAnimation(fig_sp, ims_sp, interval=50, blit=True, repeat=False)
+    ani_sp.save('Animations/Utah/Utah_spectrums.mp4')
 
     # t_ax = np.arange(1, len(traces[0]) + 1) / fs
     #
