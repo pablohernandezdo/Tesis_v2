d46475324bd035a80db97c6461dca45c422de9d6
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-07-30 17:09:44 -0400

    Change device

--- CNN/Inf_Seismic/inf_full.py
+++ CNN/Inf_Seismic/inf_full.py
@@ -22,7 +22,7 @@ def main():
     args = parser.parse_args()
 
     # Select training device
-    device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     # Load specified Classifier
     if args.classifier == 'CBN':
