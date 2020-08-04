9ecc639b025880db2bcc59d5f1fc1a4c7cf89997
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-08-03 23:16:23 -0400

    Add inference files

---
+++ CNN/Inf_Seismic/exps/inf_cbn10epch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p ../logs/CBN/inf
+
+# Full inference on cbn1epch model
+echo "Running DAS test dataset inference on cbn1epch model"
+python ../inf_full.py --model_name CBN_10epch --classifier CBN  > ../logs/CBN/inf/cbn10epch.txt
---
+++ CNN/Inf_Seismic/exps/inf_cbn10epch_v2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p ../logs/CBN_v2/inf
+
+# Full inference on cbn1epch model
+echo "Running DAS test dataset inference on cbn1epch model"
+python ../inf_full.py --model_name CBN_v2_10epch --classifier CBN_v2  > ../logs/CBN/inf/cbn10epch_v2.txt
