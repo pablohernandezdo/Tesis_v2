33b2468bf464e2cf51428d04b577fa0c55ba28e4
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-08-04 17:58:16 -0400

    Change echo messages

--- CNN/Inf_Seismic/exps/inf_cbn10epch.sh
+++ CNN/Inf_Seismic/exps/inf_cbn10epch.sh
@@ -3,5 +3,5 @@
 mkdir -p ../logs/CBN/inf
 
 # Full inference on cbn1epch model
-echo "Running DAS test dataset inference on cbn1epch model"
+echo "Running DAS test dataset inference on cbn10epch model"
 python ../inf_full.py --model_name CBN_10epch --classifier CBN  > ../logs/CBN/inf/cbn10epch.txt
--- CNN/Inf_Seismic/exps/inf_cbn1epch_V2.sh
+++ CNN/Inf_Seismic/exps/inf_cbn1epch_V2.sh
@@ -3,5 +3,5 @@
 mkdir -p ../logs/CBN_v2/inf
 
 # Full inference on cbn1epch model
-echo "Running DAS test dataset inference on cbn1epch model"
+echo "Running DAS test dataset inference on cbn1epch_v2 model"
 python ../inf_full.py --model_name CBN_v2_1epch --classifier CBN_v2  > ../logs/CBN/inf/cbn1epch_v2.txt
