d7524407fe2fcab1ba87264de17e3cb8ad228ae3
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-08-04 18:14:22 -0400

    Fix log folders

--- CNN/Inf_Seismic/exps/inf_cbn10epch_v2.sh
+++ CNN/Inf_Seismic/exps/inf_cbn10epch_v2.sh
@@ -4,4 +4,4 @@ mkdir -p ../logs/CBN_v2/inf
 
 # Full inference on cbn1epch model
 echo "Running DAS test dataset inference on cbn10epch_v2 model"
-python ../inf_full.py --model_name CBN_v2_10epch --classifier CBN_v2  > ../logs/CBN/inf/cbn10epch_v2.txt
+python ../inf_full.py --model_name CBN_v2_10epch --classifier CBN_v2  > ../logs/CBN_v2/inf/cbn10epch_v2.txt
--- CNN/Inf_Seismic/exps/inf_cbn1epch_V2.sh
+++ CNN/Inf_Seismic/exps/inf_cbn1epch_V2.sh
@@ -4,4 +4,4 @@ mkdir -p ../logs/CBN_v2/inf
 
 # Full inference on cbn1epch model
 echo "Running DAS test dataset inference on cbn1epch_v2 model"
-python ../inf_full.py --model_name CBN_v2_1epch --classifier CBN_v2  > ../logs/CBN/inf/cbn1epch_v2.txt
+python ../inf_full.py --model_name CBN_v2_1epch --classifier CBN_v2  > ../logs/CBN_V2/inf/cbn1epch_v2.txt
