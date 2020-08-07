b2efd5c824f69c7223fc9bf006a209a7f28d9b8b
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-08-07 19:51:19 -0400

    Fix

--- CNN/exps/cbn1epch_v2_validation.sh
+++ CNN/exps/cbn1epch_v2_validation.sh
@@ -8,6 +8,6 @@ val="Validation_data.hdf5"
 
 # Train CBN_v2 with evaluation for 1 epoch
 echo "Training CBN model on $trn and validating on $val"
-python ../train_validation.py --train_path $trn --validation_path $val \
+python ../train_validation.py --train_path $trn --val_path $val \
               --classifier CBN_v2 --model_name CBN_v2_1epch_train_validation \
               --n_epochs 1 --batch_size 32 --lr 1e-6  > ../logs/CBN_v2/train/CBN_v2_1epch_validation.txt
--- CNN/exps/cbn1epch_validation.sh
+++ CNN/exps/cbn1epch_validation.sh
@@ -8,6 +8,6 @@ val="Validation_data.hdf5"
 
 # Train CBN with evaluation for 1 epoch
 echo "Training CBN model on $trn and validating on $val"
-python ../train_validation.py --train_path $trn --validation_path $val \
-                    --classifier CBN --model_name CBN_1epch_validation \
-                    --n_epochs 1 --batch_size 32 --lr 1e-6  > ../logs/CBN/train/CBN_1epch_validation.txt
+python ../train_validation.py --train_path $trn --val_path $val  \
+              --classifier CBN --model_name CBN_1epch_validation \
+              --n_epochs 1 --batch_size 32 --lr 1e-6  > ../logs/CBN/train/CBN_1epch_validation.txt
