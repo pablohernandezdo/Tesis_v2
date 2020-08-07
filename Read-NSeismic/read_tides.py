e93d47af61068e5a1402f5da8fb0797b831498a2
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-08-07 01:50:43 -0400

    TRiple plot tides

--- Read-NSeismic/read_tides.py
+++ Read-NSeismic/read_tides.py
@@ -2,7 +2,9 @@ import h5py
 import numpy as np
 from numpy.random import default_rng
 
+import matplotlib.pylab as pl
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 import scipy.fftpack as sfft
 import scipy.signal as signal
@@ -13,18 +15,7 @@ from pathlib import Path
 
 def main():
     # Create images folder
-
-    Path("Imgs").mkdir(exist_ok=True)
-
-    # Carga traza STEAD
-
-    # st = '../Data_STEAD/Train_data.hdf5'
-    #
-    # with h5py.File(st, 'r') as h5_file:
-    #     grp = h5_file['earthquake']['local']
-    #     for idx, dts in enumerate(grp):
-    #         st_trace = grp[dts][:, 0] / np.max(np.abs(grp[dts][:, 0]))
-    #         break
+    Path("Imgs/Tides").mkdir(parents=True, exist_ok=True)
 
     # 1 registro, largo 259_094_163 muestras
     file = '../Data/Tides/CSULB_T13_EarthTide_earthtide_mean_360_519.mat'
@@ -48,139 +39,31 @@ def main():
     yf = sfft.fftshift(sfft.fft(trace))
 
     # Plot
-    plt.figure()
-    plt.subplot(211)
-    plt.plot(t_ax, trace)
-    plt.title('Traza dataset DAS no sísmico Tides')
-    plt.xlabel('Tiempo [s]')
-    plt.ylabel('Amplitud normalizada[-]')
-    plt.grid(True)
 
-    plt.subplot(212)
-    plt.plot(xf, np.abs(yf) / np.max(np.abs(yf)))
-    plt.xlabel('Frecuencia [Hz]')
-    plt.ylabel('Amplitud [-]')
-    plt.grid(True)
-    plt.tight_layout()
-    plt.savefig('Imgs/Tides_trace.png')
+    gs = gridspec.GridSpec(2, 2)
 
-    # data = data / np.max(np.abs(data))
-    # data_cut = data[:(data.size // 6000) * 6000]
-    # traces = data_cut.reshape((data.size // 6000, -1))
-    #
-    # t_ax = np.arange(1, len(traces[0]) + 1) / fs
-    #
-    # trace1 = traces[0]
-    # trace2 = traces[100]
-    # trace3 = traces[200]
-    #
-    # trace1_fil = butter_bandpass_filter(trace1, 0.1, 10, fs, order=3)
-    # trace2_fil = butter_bandpass_filter(trace2, 0.1, 10, fs, order=3)
-    # trace3_fil = butter_bandpass_filter(trace3, 0.1, 10, fs, order=3)
-    #
-    # trace1 = trace1 / np.max(np.abs(trace1))
-    # trace2 = trace2 / np.max(np.abs(trace2))
-    # trace3 = trace3 / np.max(np.abs(trace3))
-    #
-    # trace1_fil = trace1_fil / np.max(np.abs(trace1_fil))
-    # trace2_fil = trace2_fil / np.max(np.abs(trace2_fil))
-    # trace3_fil = trace3_fil / np.max(np.abs(trace3_fil))
-    #
-    # plt.figure()
-    # plt.subplot(311)
-    # plt.plot(t_ax, trace1)
-    # plt.grid(True)
-    # plt.ylabel('Strain [-]')
-    # plt.xlabel('Tiempo [s]')
-    # plt.title('Trazas DAS datos Tides')
-    #
-    # plt.subplot(312)
-    # plt.plot(t_ax, trace2)
-    # plt.grid(True)
-    # plt.ylabel('Strain [-]')
-    # plt.xlabel('Tiempo [s]')
-    #
-    # plt.subplot(313)
-    # plt.plot(t_ax, trace3)
-    # plt.grid(True)
-    # plt.ylabel('Strain [-]')
-    # plt.xlabel('Tiempo [s]')
-    # plt.tight_layout()
-    # plt.savefig('Imgs/TrazasDAS.png')
-    #
-    # plt.clf()
-    # plt.subplot(311)
-    # plt.plot(t_ax, trace1_fil)
-    # plt.grid(True)
-    # plt.ylabel('Strain [-]')
-    # plt.xlabel('Tiempo [s]')
-    # plt.title('Trazas DAS datos Tides filtrados 1 - 10 Hz')
-    #
-    # plt.subplot(312)
-    # plt.plot(t_ax, trace2_fil)
-    # plt.grid(True)
-    # plt.ylabel('Strain [-]')
-    # plt.xlabel('Tiempo [s]')
-    #
-    # plt.subplot(313)
-    # plt.plot(t_ax, trace3_fil)
-    # plt.grid(True)
-    # plt.ylabel('Strain [-]')
-    # plt.xlabel('Tiempo [s]')
-    # plt.tight_layout()
-    # plt.savefig('Imgs/TrazasDAS_fil.png')
-    #
-    # plt.clf()
-    # line_st, = plt.plot(trace1, label='DAS')
-    # line_das, = plt.plot(st_trace, label='STEAD')
-    # plt.grid(True)
-    # plt.xlabel('Muestras [-]')
-    # plt.ylabel('Strain [-]')
-    # plt.title('Traza STEAD y traza DAS Tides')
-    # plt.legend(handles=[line_st, line_das], loc='upper left')
-    # plt.savefig('Imgs/STEADTides.png')
-    #
-    # plt.clf()
-    # plt.subplot(211)
-    # plt.plot(st_trace)
-    # plt.grid(True)
-    # plt.xlabel('Muestras [-]')
-    # plt.ylabel('Strain [-]')
-    # plt.title('Traza STEAD y traza DAS Tides')
-    # plt.subplot(212)
-    # plt.plot(trace1)
-    # plt.grid(True)
-    # plt.xlabel('Muestras [-]')
-    # plt.ylabel('Strain [-]')
-    # plt.savefig('Imgs/STEADTides1.png')
+    pl.figure()
+    pl.subplot(gs[0, :])
+    pl.plot(t_ax, trace)
+    pl.title('Traza dataset DAS no sísmico Tides')
+    pl.xlabel('Tiempo [s]')
+    pl.ylabel('Amplitud normalizada[-]')
+    pl.grid(True)
 
+    pl.subplot(gs[1, 0])
+    pl.plot(xf, np.abs(yf) / np.max(np.abs(yf)))
+    pl.xlabel('Frecuencia [Hz]')
+    pl.ylabel('Amplitud [-]')
+    pl.grid(True)
 
-def butter_bandpass(lowcut, highcut, fs, order=5):
-    nyq = 0.5 * fs
-    low = lowcut / nyq
-    high = highcut / nyq
-    b, a = butter(order, [low, high], btype='band', output='ba')
-    return b, a
-
-
-def butter_bandpass_filter(dat, lowcut, highcut, fs, order=5):
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-    y = lfilter(b, a, dat)
-    return y
-
-
-def butter_lowpass(lowcut, highcut, fs, order=5):
-    nyq = 0.5 * fs
-    low = lowcut / nyq
-    high = highcut / nyq
-    b, a = butter(order, [low, high], output='ba')
-    return b, a
-
-
-def butter_lowpasspass_filter(dat, lowcut, highcut, fs, order=5):
-    b, a = butter_lowpass(lowcut, highcut, fs, order=order)
-    y = lfilter(b, a, dat)
-    return y
+    pl.subplot(gs[1, 1])
+    pl.plot(xf, np.abs(yf) / np.max(np.abs(yf)))
+    pl.xlim(-25, 25)
+    pl.xlabel('Frecuencia [Hz]')
+    pl.ylabel('Amplitud [-]')
+    pl.grid(True)
+    pl.tight_layout()
+    pl.savefig('Imgs/Tides/Tides_trace.png')
 
 
 if __name__ == '__main__':
