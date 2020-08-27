1228a70ab89dc3e4f81170c88856ef960f4db27d
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-08-26 23:20:03 -0400

    Add train exp

---
+++ LSTM/exps/trn.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p ../logs/LSTM/train
+mkdir -p ../logs/LSTM/eval
+mkdir -p ../models/
+
+trn="Train_data.hdf5"
+tst="Test_data.hdf5"
+
+# Train CBN for 1 epoch
+echo "Training CBN model on $trn and $tst datasets"
+python ../train.py --train_path $trn --test_path $tst \
+              --classifier C --model_name LSTM_1epch \
+              --n_epochs 1 --batch_size 32 --lr 1e-6  > ../logs/LSTM/train/LSTM_1epch.txt
+
+# Evaluate CBN on train and test datasets
+echo "Evaluating CBN model on $trn and $tst datasets"
+python ../eval.py --train_path $trn --test_path $tst \
+              --classifier C --model_name LSTM_1epch > ../logs/LSTM/eval/LSTM_1epch.txt
