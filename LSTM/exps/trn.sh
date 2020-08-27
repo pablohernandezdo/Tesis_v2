35b8f9119e0347852b3c269cb545aac1c5bc01d8
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-08-26 23:23:07 -0400

    Fix import and exp parameters

--- LSTM/exps/trn.sh
+++ LSTM/exps/trn.sh
@@ -5,15 +5,14 @@ mkdir -p ../logs/LSTM/eval
 mkdir -p ../models/
 
 trn="Train_data.hdf5"
-tst="Test_data.hdf5"
 
 # Train CBN for 1 epoch
 echo "Training CBN model on $trn and $tst datasets"
-python ../train.py --train_path $trn --test_path $tst \
+python ../train.py --train_path $trn  \
               --classifier C --model_name LSTM_1epch \
               --n_epochs 1 --batch_size 32 --lr 1e-6  > ../logs/LSTM/train/LSTM_1epch.txt
 
 # Evaluate CBN on train and test datasets
 echo "Evaluating CBN model on $trn and $tst datasets"
-python ../eval.py --train_path $trn --test_path $tst \
+python ../eval.py --train_path $trn  \
               --classifier C --model_name LSTM_1epch > ../logs/LSTM/eval/LSTM_1epch.txt
