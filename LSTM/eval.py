3a31a027ef80b3621a8835d0c0ccbc0531759350
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-09-02 20:48:32 -0400

    Change eval model name

--- LSTM/eval.py
+++ LSTM/eval.py
@@ -46,7 +46,7 @@ def main():
     # else:
     #     net = ClassConv()
     #     print('Bad Classifier option, running classifier C')
-    net = CNNLSTMANN()
+    net = Combine()
     net.to(device)
 
     # Load parameters from trained model
