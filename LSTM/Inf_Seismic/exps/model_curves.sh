59c286f9f0cff107cc5b288624ce0e16863d0f17
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-09-07 20:26:40 -0300

    Fix model

--- LSTM/Inf_Seismic/exps/model_curves.sh
+++ LSTM/Inf_Seismic/exps/model_curves.sh
@@ -4,4 +4,4 @@ mkdir -p ../logs/LSTM/inf
 
 # Test dataset inference on cbn1epch model
 echo "Running DAS test dataset inference on lstm_1e4_64 model"
-python ../inf_test_dataset.py --model_name C --classifier lstm_1e4_64  > ../logs/LSTM/inf/lstm_1e4_64.txt
+python ../inf_test_dataset.py --model_name C --classifier lstm_1e4_64_validation  > ../logs/LSTM/inf/lstm_1e4_64_validation.txt
