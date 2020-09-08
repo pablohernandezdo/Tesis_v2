13054689b89d683e25b0c2e209cdf67338bf95ab
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-09-08 15:57:25 -0300

    Change threshold

--- LSTM/Inf_Seismic/inf_test_dataset.py
+++ LSTM/Inf_Seismic/inf_test_dataset.py
@@ -62,8 +62,9 @@ def main():
     best_thresh = 0
 
     # Threshold values
+    thresholds = np.arange(0.9, 1, 0.01)
     # thresholds = np.arange(0.4, 1, 0.05)
-    thresholds = np.arange(0.1, 1, 0.1)
+    # thresholds = np.arange(0.1, 1, 0.1)
     # thresholds = np.linspace(0.05, 0.9, 18)
     # thresholds = np.linspace(0, 1, 11)
     # thresholds = np.linspace(0.4, 0.8, 5)
