ed9ef06c624e485f24474bc15c66f4df977f173e
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-09-10 00:26:17 -0300

    Fox

--- LSTM/exps/LSTM_e1pech_1e416_validation.sh
+++ LSTM/exps/LSTM_e1pech_1e416_validation.sh
@@ -9,12 +9,13 @@ trn="Train_data.hdf5"
 val="Validation_data.hdf5"
 
 # Train LSTM with evaluation for 1 epoch
-echo "Training CBN model on $trn and validating on $val"
-python ../train_validation.py --train_path $trn --val_path $val \
-              --classifier LSTM --model_name LSTM_1e4_64_validation \
-              --n_epochs 1 --batch_size 64 --lr 1e-4  > ../logs/LSTM/train/LSTM_1e4_64_validation.txt
+#echo "Training CBN model on $trn and validating on $val"
+#python ../train_validation.py --train_path $trn --val_path $val \
+#              --classifier LSTM --model_name LSTM_1e4_64_validation \
+#              --n_epochs 1 --batch_size 64 --lr 1e-4  > ../logs/LSTM/train/LSTM_1e4_64_validation.txt
 
 # Evaluate LSTM on train and test datasets
 echo "Evaluating CBN model on $trn and $tst datasets"
 python ../eval.py --train_path $trn  \
-              --classifier LSTM --model_name LSTM_1e4_64_validation > ../logs/LSTM/eval/LSTM_1e4_64_validation.txt
+              --thresh 0.5 --classifier LSTM \
+              --model_name LSTM_1e4_64_validation > ../logs/LSTM/eval/LSTM_1e4_64_validation.txt
