1259de73894b070f7bd74dbbf924278cf11a6790
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-09-10 22:34:05 -0300

    Train and eval model lstm_v2

--- LSTM/exps/models_1epch.sh
+++ LSTM/exps/models_1epch.sh
@@ -8,74 +8,74 @@ tst="Test_data.hdf5"
 trn="Train_data.hdf5"
 
 # Train LSTM for 1 epoch
-echo "Training CBN model on $trn and $tst datasets"
+echo "Training LSTM model on $trn and $tst datasets"
 python ../train.py --train_path $trn  \
               --classifier LSTM --model_name LSTM_1epch_1e4_16 \
               --n_epochs 1 --batch_size 16 --lr 1e-4  > ../logs/LSTM/train/LSTM_1epch_1e4_16.txt
 
 # Evaluate LSTM on train and test datasets
-echo "Evaluating CBN model on $trn and $tst datasets"
+echo "Evaluating LSTM model on $trn and $tst datasets"
 python ../eval.py --train_path $trn  \
               --thresh 0.5 --classifier LSTM \
               --model_name LSTM_1epch_1e4_16 > ../logs/LSTM/eval/LSTM_1epch_1e4_16.txt
 
 # Train LSTM for 1 epoch
-echo "Training CBN model on $trn and $tst datasets"
+echo "Training LSTM model on $trn and $tst datasets"
 python ../train.py --train_path $trn  \
               --classifier LSTM --model_name LSTM_1epch_1e4_32 \
               --n_epochs 1 --batch_size 32 --lr 1e-4  > ../logs/LSTM/train/LSTM_1epch_1e4_32.txt
 
 # Evaluate LSTM on train and test datasets
-echo "Evaluating CBN model on $trn and $tst datasets"
+echo "Evaluating LSTM model on $trn and $tst datasets"
 python ../eval.py --train_path $trn  \
               --thresh 0.5 --classifier LSTM \
               --model_name LSTM_1epch_1e4_32 > ../logs/LSTM/eval/LSTM_1epch_1e4_32.txt
 
 # Train LSTM for 1 epoch
-echo "Training CBN model on $trn and $tst datasets"
+echo "Training LSTM model on $trn and $tst datasets"
 python ../train.py --train_path $trn  \
               --classifier LSTM --model_name LSTM_1epch_1e4_64 \
               --n_epochs 1 --batch_size 64 --lr 1e-4  > ../logs/LSTM/train/LSTM_1epch_1e4_64.txt
 
 # Evaluate LSTM on train and test datasets
-echo "Evaluating CBN model on $trn and $tst datasets"
+echo "Evaluating LSTM model on $trn and $tst datasets"
 python ../eval.py --train_path $trn  \
               --thresh 0.5 --classifier LSTM \
               --model_name LSTM_1epch_1e4_64 > ../logs/LSTM/eval/LSTM_1epch_1e4_64.txt
 
-# Train LSTM for 1 epoch
-echo "Training CBN model on $trn and $tst datasets"
+# Train LSTM_v2 for 1 epoch
+echo "Training LSTM_v2 model on $trn and $tst datasets"
 python ../train.py --train_path $trn  \
-              --classifier LSTM --model_name LSTM_1epch_5e5_16 \
-              --n_epochs 1 --batch_size 16 --lr 5e-5  > ../logs/LSTM/train/LSTM_1epch_5e5_16.txt
+              --classifier LSTM_v2 --model_name LSTM_v2_1epch_1e4_16 \
+              --n_epochs 1 --batch_size 16 --lr 1e-4  > ../logs/LSTM/train/LSTM_v2_1epch_1e4_16.txt
 
-# Evaluate LSTM on train and test datasets
-echo "Evaluating CBN model on $trn and $tst datasets"
+# Evaluate LSTM_v2 on train and test datasets
+echo "Evaluating LSTM_v2 model on $trn and $tst datasets"
 python ../eval.py --train_path $trn  \
-              --thresh 0.5 --classifier LSTM \
-              --model_name LSTM_1epch_5e5_16 > ../logs/LSTM/eval/LSTM_1epch_5e5_16.txt
+              --thresh 0.5 --classifier LSTM_v2 \
+              --model_name LSTM_v2_1epch_1e4_16 > ../logs/LSTM/eval/LSTM_v2_1epch_1e4_16.txt
 
-# Train LSTM for 1 epoch
-echo "Training CBN model on $trn and $tst datasets"
+# Train LSTM_v2 for 1 epoch
+echo "Training LSTM_v2 model on $trn and $tst datasets"
 python ../train.py --train_path $trn  \
-              --classifier LSTM --model_name LSTM_1epch_5e5_32 \
-              --n_epochs 1 --batch_size 32 --lr 5e-5  > ../logs/LSTM/train/LSTM_1epch_5e5_32.txt
+              --classifier LSTM_v2 --model_name LSTM_v2_1epch_1e4_32 \
+              --n_epochs 1 --batch_size 32 --lr 1e-4  > ../logs/LSTM/train/LSTM_v2_1epch_1e4_32.txt
 
-# Evaluate LSTM on train and test datasets
-echo "Evaluating CBN model on $trn and $tst datasets"
+# Evaluate LSTM_v2 on train and test datasets
+echo "Evaluating LSTM_v2 model on $trn and $tst datasets"
 python ../eval.py --train_path $trn  \
-              --thresh 0.5 --classifier LSTM \
-              --model_name LSTM_1epch_5e5_32 > ../logs/LSTM/eval/LSTM_1epch_5e5_32.txt
+              --thresh 0.5 --classifier LSTM_v2 \
+              --model_name LSTM_v2_1epch_1e4_32 > ../logs/LSTM/eval/LSTM_v2_1epch_1e4_32.txt
 
-# Train CBN for 1 epoch
-echo "Training CBN model on $trn and $tst datasets"
+# Train LSTM_v2 for 1 epoch
+echo "Training LSTM_v2 model on $trn and $tst datasets"
 python ../train.py --train_path $trn  \
-              --classifier LSTM --model_name LSTM_1epch_5e5_64 \
-              --n_epochs 1 --batch_size 64 --lr 5e-5  > ../logs/LSTM/train/LSTM_1epch_5e5_64.txt
+              --classifier LSTM_v2 --model_name LSTM_v2_1epch_1e4_64 \
+              --n_epochs 1 --batch_size 64 --lr 1e-4  > ../logs/LSTM/train/LSTM_v2_1epch_1e4_64.txt
 
-# Evaluate LSTM on train and test datasets
-echo "Evaluating CBN model on $trn and $tst datasets"
+# Evaluate LSTM_v2 on train and test datasets
+echo "Evaluating LSTM_v2 model on $trn and $tst datasets"
 python ../eval.py --train_path $trn  \
-              --thresh 0.5 --classifier LSTM \
-              --model_name LSTM_1epch_5e5_64 > ../logs/LSTM/eval/LSTM_1epch_5e5_64.txt
+              --thresh 0.5 --classifier LSTM_v2 \
+              --model_name LSTM_1epch_1e4_64 > ../logs/LSTM/eval/LSTM_v2_1epch_1e4_64.txt
 
