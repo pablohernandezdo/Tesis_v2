de43ebd6400ea1f99c64007e03e115a8737be186
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-09-17 19:09:00 -0300

    Les validation

--- Build-sets/exact.sh
+++ Build-sets/exact.sh
@@ -3,4 +3,4 @@
 echo "Building train, test and validation datasets"
 python createh5_v2.py --train_traces 10240 --train_noise 10240 \
                       --test_traces 2048 --test_noise 2048   \
-                      --val_traces 2048 --val_noise 2048
+                      --val_traces 1024 --val_noise 1024
