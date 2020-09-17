e5a629d600a35ac7be953cc81cde1781f04b03c9
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-09-17 19:02:49 -0300

    Change values

--- Build-sets/exact.sh
+++ Build-sets/exact.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Building train, test and validation datasets"
-python createh5_v2.py --train_traces 5120 --train_noise 5120 \
-                      --test_traces 1024 --test_noise 1024   \
-                      --val_traces 1024 --val_noise 1024
+python createh5_v2.py --train_traces 10240 --train_noise 10240 \
+                      --test_traces 2048 --test_noise 2048   \
+                      --val_traces 2048 --val_noise 2048
