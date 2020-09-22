a9491d6517ad4841913d973bb739962116f8a4f3
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-09-21 21:55:44 -0300

    Two hidden layer pr and roc curve

--- ANN/exps/prroc.sh
+++ ANN/exps/prroc.sh
@@ -5,6 +5,6 @@ mkdir -p ../logs/eval_curves
 tst="Test_data.hdf5"
 trn="Train_data.hdf5"
 
-echo "Evaluating model 1h6k_1e4_256"
+echo "Evaluating model 2h1h5k_1e4_256"
 python ../eval_curves.py --train_path $trn --test_path $tst \
-              --classifier 1h6k --model_name 1h6k_1e4_256 > ../logs/eval_curves/1h6k_1e4_256.txt
+              --classifier 2h1h5k --model_name 2h1h5k_1e4_256 > ../logs/eval_curves/2h1h5k_1e4_256.txt
