46c9178860edfff691c23886af46063d87068e26
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-10-03 12:41:02 -0300

    1 exp early stop

--- ANN/exps/twohidden.sh
+++ ANN/exps/twohidden.sh
@@ -8,17 +8,17 @@ tst="Test_data.hdf5"
 trn="Train_data.hdf5"
 val="Validation_data.hdf5"
 
-#echo "Training model 2h6k6k, lr = 1e-4, epochs = 5, batch_size = 256"
-#python ../train_validation.py \
-#              --train_path $trn --val_path $val      \
-#              --n_epochs 5 --lr 1e-4 --batch_size 256 \
-#              --classifier 2h6k6k --model_name 2h6k6k_1e4_256 > ../logs/train/2h6k6k_1e4_256.txt
-#
-#echo "Evaluating model 2h6k6k_1e4_256"
-#python ../eval.py --train_path $trn --test_path $tst \
-#              --classifier 2h6k6k --model_name 2h6k6k_1e4_256 > ../logs/eval/2h6k6k_1e4_256.txt
-#
-#
+echo "Training model 2h6k6k, lr = 1e-4, epochs = 5, batch_size = 256"
+python ../train_validation.py \
+              --train_path $trn --val_path $val      \
+              --n_epochs 5 --lr 1e-4 --batch_size 256 \
+              --classifier 2h6k6k --model_name 2h6k6k_1e4_256 > ../logs/train/2h6k6k_1e4_256.txt
+
+echo "Evaluating model 2h6k6k_1e4_256"
+python ../eval.py --train_path $trn --test_path $tst \
+              --classifier 2h6k6k --model_name 2h6k6k_1e4_256 > ../logs/eval/2h6k6k_1e4_256.txt
+
+
 #echo "Training model 2h6k5k, lr = 1e-4, epochs = 5, batch_size = 256"
 #python ../train_validation.py \
 #              --train_path $trn --val_path $val      \
