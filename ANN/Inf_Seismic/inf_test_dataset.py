c606fdcfe1aad02050fec7101beb8c8ed84c46b3
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-10-05 19:59:20 -0300

    More thresholdsss

--- ANN/Inf_Seismic/inf_test_dataset.py
+++ ANN/Inf_Seismic/inf_test_dataset.py
@@ -53,9 +53,8 @@ def main():
     best_thresh = 0
 
     # Threshold values
-    thresholds = [0.5]
     # thresholds = np.arange(0.4, 1, 0.05)
-    # thresholds = np.arange(0.1, 1, 0.1)
+    thresholds = np.arange(0.1, 1, 0.1)
     # thresholds = np.linspace(0.05, 0.9, 18)
     # thresholds = np.linspace(0, 1, 11)
     # thresholds = np.linspace(0.4, 0.8, 5)
