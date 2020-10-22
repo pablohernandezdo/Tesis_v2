1cef2a518a4f6eced8ce918c0a7f88e70a2df1e0
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-10-22 17:34:28 -0300

    no ealystop

--- ANN/exps/step1.sh
+++ ANN/exps/step1.sh
@@ -11,6 +11,7 @@ val="Validation_data.hdf5"
 echo "Training model 1h3k, lr = 1e-3, epochs = 5, batch_size = 256"
 python ../train_validation.py \
               --train_path $trn --val_path $val      \
+              --earlystop 0  \
               --n_epochs 5 --lr 1e-3 --batch_size 256 \
               --classifier 1h3k --model_name 1h3k_1e3_256 > ../logs/train/1h3k_1e3_256.txt
 
@@ -21,6 +22,7 @@ python ../eval.py --train_path $trn --test_path $tst \
 echo "Training model 1h3k, lr = 1e-4, epochs = 5, batch_size = 256"
 python ../train_validation.py \
               --train_path $trn --val_path $val      \
+              --earlystop 0  \
               --n_epochs 5 --lr 1e-4 --batch_size 256 \
               --classifier 1h3k --model_name 1h3k_1e4_256 > ../logs/train/1h3k_1e4_256.txt
 
@@ -31,6 +33,7 @@ python ../eval.py --train_path $trn --test_path $tst \
 echo "Training model 1h3k, lr = 1e-5, epochs = 5, batch_size = 256"
 python ../train_validation.py \
               --train_path $trn --val_path $val      \
+              --earlystop 0  \
               --n_epochs 5 --lr 1e-5 --batch_size 256 \
               --classifier 1h3k --model_name 1h3k_1e5_256 > ../logs/train/1h3k_1e5_256.txt
 
@@ -41,6 +44,7 @@ python ../eval.py --train_path $trn --test_path $tst \
 echo "Training model 1h3k, lr = 1e-6, epochs = 5, batch_size = 256"
 python ../train_validation.py \
               --train_path $trn --val_path $val      \
+              --earlystop 0  \
               --n_epochs 5 --lr 1e-6 --batch_size 256 \
               --classifier 1h3k --model_name 1h3k_1e6_256 > ../logs/train/1h3k_1e6_256.txt
 
