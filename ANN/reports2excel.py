ded87d66200643979f4014d6eade88a590e4e98d
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-11-02 13:10:51 -0300

    try excel automatization

---
+++ ANN/reports2excel.py
@@ -0,0 +1,34 @@
+import os
+import argparse
+
+import numpy as np
+import pandas as pd
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--folder_name', default='train', help='Name of folder to read log files')
+    parser.add_argument('--xls_name', default='train_xls', help='Name of excel file to export')
+    parser.add_argument('--n_thresh', type=int, default=10, help='Number of thresholds evaluated')
+    args = parser.parse_args()
+
+    # Leer los archivos de la carpeta
+    files = os.listdir(os.path.join('logs', args.folder_name))
+
+    print(files)
+    
+    # Por cada archivo leer las lineas y extraer
+    # la informacion importante, añadirla a un dataframe
+    # Guardar el excel
+
+    # df = pd.DataFrame({
+    #     'Col A': [1, 2, 3],
+    #     'Col B': ['A', 'B', 'C'],
+    #     'Col C': ['Hola', 'Compas', 'Compitas'],
+    # })
+    #
+    # df.to_excel('df.xlsx', index=False)
+
+
+if __name__ == "__main__":
+    main()
