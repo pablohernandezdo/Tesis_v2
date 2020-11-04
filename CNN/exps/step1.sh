4a4a7e4bf9a708a119915da82db8eddd6b3ac286
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-11-04 18:39:12 -0300

    :(

--- CNN/exps/step1.sh
+++ CNN/exps/step1.sh
@@ -10,16 +10,16 @@ val="Validation_data.hdf5"
 
 # Type 1, 1 hidden layer models
 
-#echo "Training model 1c3k, lr = 1e-3, epochs = 5, batch_size = 256"
-#python ../train_validation.py \
-#              --train_path $trn --val_path $val      \
-#              --earlystop 0  \
-#              --n_epochs 5 --lr 1e-3 --batch_size 256 \
-#              --classifier 1c3k --model_name 1c3k_1e3_256 > ../logs/train/1c3k_1e3_256.txt
-#
-#echo "Evaluating model 1c3k_1e3_256"
-#python ../eval.py --train_path $trn --test_path $tst \
-#              --classifier 1c3k --model_name 1c3k_1e3_256 > ../logs/eval/1c3k_1e3_256.txt
+echo "Training model 1c3k, lr = 1e-3, epochs = 5, batch_size = 256"
+python ../train_validation.py \
+              --train_path $trn --val_path $val      \
+              --earlystop 0  \
+              --n_epochs 5 --lr 1e-3 --batch_size 256 \
+              --classifier 1c3k --model_name 1c3k_1e3_256 > ../logs/train/1c3k_1e3_256.txt
+
+echo "Evaluating model 1c3k_1e3_256"
+python ../eval.py --train_path $trn --test_path $tst \
+              --classifier 1c3k --model_name 1c3k_1e3_256 > ../logs/eval/1c3k_1e3_256.txt
 
 #echo "Training model 1c3k, lr = 1e-4, epochs = 5, batch_size = 256"
 #python ../train_validation.py \
