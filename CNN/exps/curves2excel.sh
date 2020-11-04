0542fc9a0d474650af8ebe2e479a97e0120d70fc
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-11-04 20:06:19 -0300

    fix ?|

--- CNN/exps/curves2excel.sh
+++ CNN/exps/curves2excel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Creating summary of reports excel file"
-python ../reports2excel.py --xls_name 'step1CNN_curves' --n_thresh 19
+python ../reports2excel.py --folder_name 'eval' --xls_name 'step1CNN_curves' --n_thresh 19
