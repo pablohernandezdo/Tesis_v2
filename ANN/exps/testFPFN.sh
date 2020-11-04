614ede87f3bb5c776708c17ec582cfded705391d
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-11-04 20:15:00 -0300

    fix

--- ANN/exps/testFPFN.sh
+++ ANN/exps/testFPFN.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+mkdir -p ../logs/train
+mkdir -p ../logs/eval
+mkdir -p ../models
+
+tst="Test_data.hdf5"
+
 echo "Evaluating model 2h1k5k_5e5_256"
 python ../eval_curves.py --test_path $tst \
               --classifier 2h1k5k --model_name 2h1k5k_5e5_256_40 > ../logs/eval/2h1k5k_5e5_256_40.txt
