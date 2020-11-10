6aefeded46457df018b67c0e7afac9b696f88f67
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-11-10 12:15:00 -0300

    fix folder to save logs

--- ANN/exps/step1.sh
+++ ANN/exps/step1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p ../logs/train/step1
-mkdir -p ../logs/eval/step1
+mkdir -p ../logs/train/step1
 mkdir -p ../models/step1
 
 tst="Test_data.hdf5"
@@ -15,48 +15,48 @@ python ../train_validation.py \
               --train_path $trn --val_path $val      \
               --earlystop 0 --model_folder step1 \
               --n_epochs 5 --lr 1e-3 --batch_size 256 \
-              --classifier 1h3k --model_name 1h3k_1e3_256 > ../logs/train/1h3k_1e3_256.txt
+              --classifier 1h3k --model_name 1h3k_1e3_256 > ../logs/train/step1/1h3k_1e3_256.txt
 
 echo "Evaluating model 1h3k_1e3_256"
 python ../eval.py --train_path $trn --test_path $tst \
               --model_folder step1 \
-              --classifier 1h3k --model_name 1h3k_1e3_256 > ../logs/eval/1h3k_1e3_256.txt
+              --classifier 1h3k --model_name 1h3k_1e3_256 > ../logs/train/step1/1h3k_1e3_256.txt
 
 echo "Training model 1h3k, lr = 1e-4, epochs = 5, batch_size = 256"
 python ../train_validation.py \
               --train_path $trn --val_path $val      \
               --earlystop 0 --model_folder step1 \
               --n_epochs 5 --lr 1e-4 --batch_size 256 \
-              --classifier 1h3k --model_name 1h3k_1e4_256 > ../logs/train/1h3k_1e4_256.txt
+              --classifier 1h3k --model_name 1h3k_1e4_256 > ../logs/train/step1/1h3k_1e4_256.txt
 
 echo "Evaluating model 1h3k_1e4_256"
 python ../eval.py --train_path $trn --test_path $tst \
               --model_folder step1 \
-              --classifier 1h3k --model_name 1h3k_1e4_256 > ../logs/eval/1h3k_1e4_256.txt
+              --classifier 1h3k --model_name 1h3k_1e4_256 > ../logs/train/step1/1h3k_1e4_256.txt
 
 echo "Training model 1h3k, lr = 1e-5, epochs = 5, batch_size = 256"
 python ../train_validation.py \
               --train_path $trn --val_path $val      \
               --earlystop 0 --model_folder step1 \
               --n_epochs 5 --lr 1e-5 --batch_size 256 \
-              --classifier 1h3k --model_name 1h3k_1e5_256 > ../logs/train/1h3k_1e5_256.txt
+              --classifier 1h3k --model_name 1h3k_1e5_256 > ../logs/train/step1/1h3k_1e5_256.txt
 
 echo "Evaluating model 1h3k_1e5_256"
 python ../eval.py --train_path $trn --test_path $tst \
               --model_folder step1 \
-              --classifier 1h3k --model_name 1h3k_1e5_256 > ../logs/eval/1h3k_1e5_256.txt
+              --classifier 1h3k --model_name 1h3k_1e5_256 > ../logs/train/step1/1h3k_1e5_256.txt
 
 echo "Training model 1h3k, lr = 1e-6, epochs = 5, batch_size = 256"
 python ../train_validation.py \
               --train_path $trn --val_path $val      \
               --earlystop 0 --model_folder step1 \
               --n_epochs 5 --lr 1e-6 --batch_size 256 \
-              --classifier 1h3k --model_name 1h3k_1e6_256 > ../logs/train/1h3k_1e6_256.txt
+              --classifier 1h3k --model_name 1h3k_1e6_256 > ../logs/train/step1/1h3k_1e6_256.txt
 
 echo "Evaluating model 1h3k_1e6_256"
 python ../eval.py --train_path $trn --test_path $tst \
               --model_folder step1 \
-              --classifier 1h3k --model_name 1h3k_1e6_256 > ../logs/eval/1h3k_1e6_256.txt
+              --classifier 1h3k --model_name 1h3k_1e6_256 > ../logs/train/step1/1h3k_1e6_256.txt
 
 # 2 hidden layer models
 
@@ -65,48 +65,48 @@ python ../train_validation.py \
               --train_path $trn --val_path $val      \
               --earlystop 0 --model_folder step1 \
               --n_epochs 5 --lr 1e-3 --batch_size 256 \
-              --classifier 2h3k3k --model_name 2h3k3k_1e3_256 > ../logs/train/2h3k3k_1e3_256.txt
+              --classifier 2h3k3k --model_name 2h3k3k_1e3_256 > ../logs/train/step1/2h3k3k_1e3_256.txt
 
 echo "Evaluating model 2h3k3k_1e3_256"
 python ../eval.py --train_path $trn --test_path $tst \
               --model_folder step1 \
-              --classifier 2h3k3k --model_name 2h3k3k_1e3_256 > ../logs/eval/2h3k3k_1e3_256.txt
+              --classifier 2h3k3k --model_name 2h3k3k_1e3_256 > ../logs/train/step1/2h3k3k_1e3_256.txt
 
 echo "Training model 2h3k3k, lr = 1e-4, epochs = 5, batch_size = 256"
 python ../train_validation.py \
               --train_path $trn --val_path $val      \
               --earlystop 0 --model_folder step1 \
               --n_epochs 5 --lr 1e-4 --batch_size 256 \
-              --classifier 2h3k3k --model_name 2h3k3k_1e4_256 > ../logs/train/2h3k3k_1e4_256.txt
+              --classifier 2h3k3k --model_name 2h3k3k_1e4_256 > ../logs/train/step1/2h3k3k_1e4_256.txt
 
 echo "Evaluating model 2h3k3k_1e4_256"
 python ../eval.py --train_path $trn --test_path $tst \
               --model_folder step1 \
-              --classifier 2h3k3k --model_name 2h3k3k_1e4_256 > ../logs/eval/2h3k3k_1e4_256.txt
+              --classifier 2h3k3k --model_name 2h3k3k_1e4_256 > ../logs/train/step1/2h3k3k_1e4_256.txt
 
 echo "Training model 2h3k3k, lr = 1e-5, epochs = 5, batch_size = 256"
 python ../train_validation.py \
               --train_path $trn --val_path $val      \
               --earlystop 0 --model_folder step1 \
               --n_epochs 5 --lr 1e-5 --batch_size 256 \
-              --classifier 2h3k3k --model_name 2h3k3k_1e5_256 > ../logs/train/2h3k3k_1e5_256.txt
+              --classifier 2h3k3k --model_name 2h3k3k_1e5_256 > ../logs/train/step1/2h3k3k_1e5_256.txt
 
 echo "Evaluating model 2h3k3k_1e5_256"
 python ../eval.py --train_path $trn --test_path $tst \
               --model_folder step1 \
-              --classifier 2h3k3k --model_name 2h3k3k_1e5_256 > ../logs/eval/2h3k3k_1e5_256.txt
+              --classifier 2h3k3k --model_name 2h3k3k_1e5_256 > ../logs/train/step1/2h3k3k_1e5_256.txt
 
 echo "Training model 2h3k3k, lr = 1e-6, epochs = 5, batch_size = 256"
 python ../train_validation.py \
               --train_path $trn --val_path $val      \
               --earlystop 0 --model_folder step1 \
               --n_epochs 5 --lr 1e-6 --batch_size 256 \
-              --classifier 2h3k3k --model_name 2h3k3k_1e6_256 > ../logs/train/2h3k3k_1e6_256.txt
+              --classifier 2h3k3k --model_name 2h3k3k_1e6_256 > ../logs/train/step1/2h3k3k_1e6_256.txt
 
 echo "Evaluating model 2h3k3k_1e6_256"
 python ../eval.py --train_path $trn --test_path $tst \
               --model_folder step1 \
-              --classifier 2h3k3k --model_name 2h3k3k_1e6_256 > ../logs/eval/2h3k3k_1e6_256.txt
+              --classifier 2h3k3k --model_name 2h3k3k_1e6_256 > ../logs/train/step1/2h3k3k_1e6_256.txt
 
 # reports 2 excel
 
