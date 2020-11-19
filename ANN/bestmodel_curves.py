604fe351cede5874b7ff032db08ed6be70fe17e1
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-11-19 12:56:14 -0300

    Plots for best ann models

--- ANN/bestmodel_curves.py
+++ ANN/bestmodel_curves.py
@@ -40,7 +40,7 @@ def main():
     roc_aucs = []
 
     for f_name in models:
-        with open(os.path.join(eval_wkdir, models), 'r') as f:
+        with open(os.path.join(eval_wkdir, f_name), 'r') as f:
 
             f.readline()
             f.readline()
@@ -73,9 +73,9 @@ def main():
                 fsc.append(f.readline().split(":")[-1].strip())
 
             # Aqui armar la curva y agregarlas a la lista mayor
-            pr_curves.append([pre, rec])
-            roc_curves.append([fpr, rec,])
-            fscore_curves.append([fsc, thresholds])
+            pr_curves.append([rec, pre])
+            roc_curves.append([fpr, rec])
+            fscore_curves.append([thresholds, fsc])
 
         pre = []
         rec = []
@@ -83,7 +83,51 @@ def main():
         fsc = []
         thresholds = []
 
-    # Aqui graficar todas las curvas y guardarlas en una carpeta
+    # Curvas PR
+    plt.figure()
+
+    for crv in pr_curves:
+        plt.plot(crv[0], crv[1])
+
+    # Dumb model line
+    plt.hlines(0.5, 0, 1, 'b', '--')
+    plt.title(f'PR curves best models ANN')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.xlim(-0.02, 1.02)
+    plt.ylim(0.48, 1.02)
+    plt.grid(True)
+    plt.savefig(f'Analysis/Curves_parameters/best_PR_ann.png')
+
+    # Curva ROC
+    plt.figure()
+
+    for crv in roc_curves:
+        plt.plot(crv[0], crv[1])
+
+    # Dumb model line
+    plt.plot([0, 1], [0, 1], 'b--')
+    plt.title(f'ROC curves best models ANN')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('Recall')
+    plt.xlim(-0.02, 1.02)
+    plt.ylim(-0.02, 1.02)
+    plt.grid(True)
+    plt.savefig(f'Analysis/Curves_parameters/best_ROC_ann.png')
+
+    # Curva Fscore
+    plt.figure()
+
+    for crv in fscore_curves:
+        plt.plot(crv[0], crv[1])
+
+    plt.title(f'Fscore vs thresholds curves best models ANN')
+    plt.xlabel('Threshold')
+    plt.ylabel('F-score')
+    plt.xlim(-0.02, 1.02)
+    plt.ylim(-0.02, 1.02)
+    plt.grid(True)
+    plt.savefig(f'Analysis/Curves_parameters/best_Fscore_ann.png')
 
 
 if __name__ == "__main__":
