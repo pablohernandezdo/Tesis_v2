ff415ac87be4735878451738776627074e726c89
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-11-19 13:12:20 -0300

    modo mono

--- ANN/exps/bestmodel_curves.sh
+++ ANN/exps/bestmodel_curves.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-mdl="2h2k5k_5e5_256_25.txt"
-mdl="mdl 2h1k5k_5e5_256_20.txt"
-mdl="mdl 2h1k6k_5e5_256_20.txt"
-mdl="mdl 2h1k4k_5e5_256_25.txt"
-mdl="mdl 2h1k6k_5e5_256_30.txt"
-mdl="mdl 2h2k5k_5e5_256_20.txt"
-mdl="mdl 2h5h4k_5e5_256_35.txt"
-mdl="mdl 2h1k6k_1e4_256_20.txt"
-mdl="mdl 2h5h6k_5e5_256_25.txt"
-mdl="mdl 22h1k4k_1e4_256_20.txt"
-
 # Get best models PR, ROC and Fscore curves
 python ../bestmodel_curves.py --archives_folder 'step4_curves' \
-                              --best_models $mdl
+                              --best_models "2h2k5k_5e5_256_25.txt 2h1k5k_5e5_256_20.txt 2h1k6k_5e5_256_20.txt 2h1k4k_5e5_256_25.txt 2h1k6k_5e5_256_30.txt 2h2k5k_5e5_256_20.txt 2h5h4k_5e5_256_35.txt 2h1k6k_1e4_256_20.txt 2h5h6k_5e5_256_25.txt 2h1k4k_1e4_256_20.txt"
