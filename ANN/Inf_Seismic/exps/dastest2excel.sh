e2520b70094a3ff57a40243319b04d8392c135c5
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-11-21 17:13:25 -0300

    Fix step 5 excel reports

--- ANN/Inf_Seismic/exps/dastest2excel.sh
+++ ANN/Inf_Seismic/exps/dastest2excel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Creating summary of reports excel file"
-python ../dastest2excel.py --xls_name 'dastest'
+python ../dastest2excel.py --xls_name 'ANN_step5' --archives_folder 'itd'
