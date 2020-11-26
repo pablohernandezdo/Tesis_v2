7e1d9083c6962d3c48aed4618a5bd133ad531034
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-11-25 22:19:22 -0300

    clf

--- ANN/final_analysis.py
+++ ANN/final_analysis.py
@@ -317,7 +317,7 @@ def main():
     plt.savefig(f'../Analysis/Final/Averages/best_PR_ann.png')
 
     # Curva ROC
-    plt.figure()
+    plt.clf()
 
     for crv in avg_roc_curves:
         plt.plot(crv[0], crv[1])
@@ -336,7 +336,7 @@ def main():
     plt.savefig(f'../Analysis/Final/Averages/best_ROC_ann.png')
 
     # Curva Fscore
-    plt.figure()
+    plt.clf()
 
     for crv in avg_fscore_curves:
         plt.plot(crv[0], crv[1])
@@ -355,7 +355,7 @@ def main():
     # Comparar las curvas PR de STEAD Y DAS
     for (pr_step4, pr_step5, mdl) in zip(step4_pr_curves, step5_pr_curves, best_models):
         mdl = mdl.split('.')[0].strip()
-        plt.figure()
+        plt.clf()
 
         line_st4, = plt.plot(pr_step4[0], pr_step4[1], label="STEAD PR curve")
         line_st5, = plt.plot(pr_step5[0], pr_step5[1], label="DAS PR curve")
@@ -372,7 +372,7 @@ def main():
     # Comparar las curvas ROC de STEAD Y DAS
     for (roc_step4, roc_step5, mdl) in zip(step4_roc_curves, step5_roc_curves, best_models):
         mdl = mdl.split('.')[0].strip()
-        plt.figure()
+        plt.clf()
 
         line_st4, = plt.plot(roc_step4[0], roc_step4[1], label="STEAD ROC curve")
         line_st5, = plt.plot(roc_step5[0], roc_step5[1], label="DAS ROC curve")
@@ -389,7 +389,7 @@ def main():
     # Comparar las curvas Fscore de STEAD Y DAS
     for (fscore_step4, fscore_step5, mdl) in zip(step4_fscore_curves, step5_fscore_curves, best_models):
         mdl = mdl.split('.')[0].strip()
-        plt.figure()
+        plt.clf()
 
         line_st4, = plt.plot(fscore_step4[0], fscore_step4[1], label="STEAD Fscore curve")
         line_st5, = plt.plot(fscore_step5[0], fscore_step5[1], label="DAS Fscore curve")
