b8d9fabc820d2e302aa0084440874788157f25a0
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-11-29 13:41:25 -0300

    Print fname

--- CNN/exps/final_analysis.sh
+++ CNN/exps/final_analysis.sh
@@ -3,6 +3,4 @@
 # Get best models PR, ROC and Fscore curves
 python ../final_analysis.py --step4_folder 'step4' \
                             --step5_folder 'step5' \
-                            # --avg_folder 'avgmodels' \
                             --best_models "Cnn1_2k_1h_5e5_256_40.txt Cnn1_3k_10_1e4_256_40.txt Cnn1_3k_10_1e4_256_25.txt Cnn1_3k_10_1e4_256_40.txt Cnn1_2k_1h_5e5_256_40.txt Cnn1_6k_2h_1e4_256_30.txt Cnn1_3k_10_1e4_256_25.txt Cnn1_3k_10_1e4_256_40.txt Cnn1_2k_1h_5e5_256_40.txt Cnn1_6k_2h_1e4_256_30.txt" \
-                            # --avg_models "2h1h10_1e4_256_30.txt 1h5h_1e4_256_30.txt 2h10_2k_1e4_256_30.txt 2h1_2k_1e4_256_30.txt 2h5h10_1e4_256_30.txt 2h1h1h_1e4_256_30.txt 2h10_1k_1e4_256_30.txt 1h1k_1e4_256_30.txt 2h5h1h_1e4_256_30.txt 2h1h1k_1e4_256_30.txt"
--- CNN/final_analysis.py
+++ CNN/final_analysis.py
@@ -147,6 +147,7 @@ def main():
     # avg_roc_aucs = list(map(float, avg_roc_aucs))
 
     for f_name in best_models:
+        print(f_name)
         with open(os.path.join(step4_eval_wkdir, f_name), 'r') as f:
 
             f.readline()
