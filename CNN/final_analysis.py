19a0ec042ce064e5d1d09e6d6b0144b48eeacc77
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-11-29 13:58:00 -0300

    print mdl

--- CNN/final_analysis.py
+++ CNN/final_analysis.py
@@ -359,6 +359,7 @@ def main():
     # Comparar las curvas PR de STEAD Y DAS
     for (pr_step4, pr_step5, mdl) in zip(step4_pr_curves, step5_pr_curves, best_models):
         mdl = mdl.split('.')[0].strip()
+        print(mdl)
         plt.clf()
 
         line_st4, = plt.plot(pr_step4[0], pr_step4[1], label="STEAD PR curve")
@@ -373,39 +374,39 @@ def main():
         plt.legend(handles=[line_st4, line_st5], loc='best')
         plt.savefig(f'../Analysis/Final/PR_curves/Comparacion_PR_{mdl}.png')
 
-    # Comparar las curvas ROC de STEAD Y DAS
-    for (roc_step4, roc_step5, mdl) in zip(step4_roc_curves, step5_roc_curves, best_models):
-        mdl = mdl.split('.')[0].strip()
-        plt.clf()
-
-        line_st4, = plt.plot(roc_step4[0], roc_step4[1], label="STEAD ROC curve")
-        line_st5, = plt.plot(roc_step5[0], roc_step5[1], label="DAS ROC curve")
-
-        plt.title(f'Comparacion curva ROC STEAD y DAS modelo {mdl}')
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('Recall')
-        plt.xlim(-0.02, 1.02)
-        plt.ylim(-0.02, 1.02)
-        plt.grid(True)
-        plt.legend(handles=[line_st4, line_st5], loc='best')
-        plt.savefig(f'../Analysis/Final/ROC_curves/Comparacion_ROC_{mdl}.png')
-
-    # Comparar las curvas Fscore de STEAD Y DAS
-    for (fscore_step4, fscore_step5, mdl) in zip(step4_fscore_curves, step5_fscore_curves, best_models):
-        mdl = mdl.split('.')[0].strip()
-        plt.clf()
-
-        line_st4, = plt.plot(fscore_step4[0], fscore_step4[1], label="STEAD Fscore curve")
-        line_st5, = plt.plot(fscore_step5[0], fscore_step5[1], label="DAS Fscore curve")
-
-        plt.title(f'Comparacion curva Fscore STEAD y DAS modelo {mdl}')
-        plt.xlabel('Umbral')
-        plt.ylabel('Fscore')
-        plt.xlim(-0.02, 1.02)
-        plt.ylim(-0.02, 1.02)
-        plt.grid(True)
-        plt.legend(handles=[line_st4, line_st5], loc='best')
-        plt.savefig(f'../Analysis/Final/Fscore_curves/Comparacion_Fscore_{mdl}.png')
+    # # Comparar las curvas ROC de STEAD Y DAS
+    # for (roc_step4, roc_step5, mdl) in zip(step4_roc_curves, step5_roc_curves, best_models):
+    #     mdl = mdl.split('.')[0].strip()
+    #     plt.clf()
+    #
+    #     line_st4, = plt.plot(roc_step4[0], roc_step4[1], label="STEAD ROC curve")
+    #     line_st5, = plt.plot(roc_step5[0], roc_step5[1], label="DAS ROC curve")
+    #
+    #     plt.title(f'Comparacion curva ROC STEAD y DAS modelo {mdl}')
+    #     plt.xlabel('False Positive Rate')
+    #     plt.ylabel('Recall')
+    #     plt.xlim(-0.02, 1.02)
+    #     plt.ylim(-0.02, 1.02)
+    #     plt.grid(True)
+    #     plt.legend(handles=[line_st4, line_st5], loc='best')
+    #     plt.savefig(f'../Analysis/Final/ROC_curves/Comparacion_ROC_{mdl}.png')
+    #
+    # # Comparar las curvas Fscore de STEAD Y DAS
+    # for (fscore_step4, fscore_step5, mdl) in zip(step4_fscore_curves, step5_fscore_curves, best_models):
+    #     mdl = mdl.split('.')[0].strip()
+    #     plt.clf()
+    #
+    #     line_st4, = plt.plot(fscore_step4[0], fscore_step4[1], label="STEAD Fscore curve")
+    #     line_st5, = plt.plot(fscore_step5[0], fscore_step5[1], label="DAS Fscore curve")
+    #
+    #     plt.title(f'Comparacion curva Fscore STEAD y DAS modelo {mdl}')
+    #     plt.xlabel('Umbral')
+    #     plt.ylabel('Fscore')
+    #     plt.xlim(-0.02, 1.02)
+    #     plt.ylim(-0.02, 1.02)
+    #     plt.grid(True)
+    #     plt.legend(handles=[line_st4, line_st5], loc='best')
+    #     plt.savefig(f'../Analysis/Final/Fscore_curves/Comparacion_Fscore_{mdl}.png')
 
 
 if __name__ == "__main__":
