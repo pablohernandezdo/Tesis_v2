972877279dbca7606d8e2b72494335615e61403f
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-12-17 01:33:53 -0300

    No legend

--- ANN/final_analysis.py
+++ ANN/final_analysis.py
@@ -581,7 +581,7 @@ def main():
     plt.xlim(-0.02, 1.02)
     plt.ylim(0.48, 1.02)
     plt.grid(True)
-    plt.legend(loc='best')
+    # plt.legend(loc='best')
     plt.savefig(f'../Analysis/Final/Averages_comp/avgcomp_PR_ann.png')
 
     # Curva ROC
