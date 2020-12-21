854f079ace984dcd53e392fa43f76b23296f74f6
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-12-21 19:41:12 -0300

    de nuevo

--- ANN/final_analysis.py
+++ ANN/final_analysis.py
@@ -491,8 +491,8 @@ def main():
     plt.grid(True)
     plt.legend(loc='best', frameon=False)
     plt.savefig(f'../Analysis/Final/Best/best_ROC_ann.png')
-    plt.xlim(-0.01, 0.2)
-    plt.ylim(0.8, 1.01)
+    plt.xlim(-0.01, 0.1)
+    plt.ylim(0.9, 1.01)
     plt.legend(loc='best', frameon=False)
     plt.savefig(f'../Analysis/Final/Best/best_ROC_ann_zoom.png')
 
