bf153c2aa25a8f04b7fda46412fee0762bed372e
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-12-21 19:40:16 -0300

    Fix lims

--- ANN/final_analysis.py
+++ ANN/final_analysis.py
@@ -491,7 +491,7 @@ def main():
     plt.grid(True)
     plt.legend(loc='best', frameon=False)
     plt.savefig(f'../Analysis/Final/Best/best_ROC_ann.png')
-    plt.xlim(-0.01, 0.02)
+    plt.xlim(-0.01, 0.2)
     plt.ylim(0.8, 1.01)
     plt.legend(loc='best', frameon=False)
     plt.savefig(f'../Analysis/Final/Best/best_ROC_ann_zoom.png')
