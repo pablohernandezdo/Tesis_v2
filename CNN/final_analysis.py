3927e95b6b76a7b7ea6b6fdc6584e777dad80b45
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-12-22 00:53:27 -0300

    new lims 2

--- CNN/final_analysis.py
+++ CNN/final_analysis.py
@@ -313,7 +313,7 @@ def main():
     plt.grid(True)
     plt.legend(loc='best', frameon=False)
     plt.savefig(f'../Analysis/Final/Best/best_PR_cnn.png')
-    plt.xlim(0.98, 1.01)
+    plt.xlim(0.99, 1.005)
     plt.ylim(0.9, 1.01)
     plt.legend(loc='best', frameon=False)
     plt.savefig(f'../Analysis/Final/Best/best_PR_cnn_zoom.png')
