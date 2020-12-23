59ccd52c6f967d0b98388e39ffc84424de1f4a40
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-12-23 18:32:32 -0300

    Fix bins

--- CNN/Inf_Seismic/exps/output2histogram.sh
+++ CNN/Inf_Seismic/exps/output2histogram.sh
@@ -4,10 +4,10 @@ fld="step4"
 
 python ../output2histogram.py --model_folder $fld --model_name "Cnn1_1k_2k_1e4_256_20" --n_bins 30
 
-python ../output2histogram.py --model_folder $fld --model_name "Cnn1_2k_1h_5e4_256_40" --n_bins 35
+python ../output2histogram.py --model_folder $fld --model_name "Cnn1_2k_1h_5e4_256_40" --n_bins 30
 
-python ../output2histogram.py --model_folder $fld --model_name "Cnn1_2k_1h_1e4_256_30" --n_bins 40
+python ../output2histogram.py --model_folder $fld --model_name "Cnn1_2k_1h_1e4_256_30" --n_bins 30
 
-python ../output2histogram.py --model_folder $fld --model_name "Cnn1_2k_1h_5e5_256_40" --n_bins 45
+python ../output2histogram.py --model_folder $fld --model_name "Cnn1_2k_1h_5e5_256_40" --n_bins 30
 
-python ../output2histogram.py --model_folder $fld --model_name "Cnn1_3k_10_1e4_256_25" --n_bins 50
+python ../output2histogram.py --model_folder $fld --model_name "Cnn1_3k_10_1e4_256_25" --n_bins 30
