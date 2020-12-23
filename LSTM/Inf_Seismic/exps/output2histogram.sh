e95ccd3f83a942c845e2edfa059ef2ca8ed207c1
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-12-23 19:13:12 -0300

    50 bins

--- LSTM/Inf_Seismic/exps/output2histogram.sh
+++ LSTM/Inf_Seismic/exps/output2histogram.sh
@@ -2,12 +2,12 @@
 
 fld="step5"
 
-python ../output2histogram.py --model_folder $fld --model_name "Lstm_128_32_1_1_5e4_256_20" --n_bins 30
+python ../output2histogram.py --model_folder $fld --model_name "Lstm_128_32_1_1_5e4_256_20" --n_bins 50
 
-python ../output2histogram.py --model_folder $fld --model_name "Lstm_128_32_2_1_5e4_256_35" --n_bins 30
+python ../output2histogram.py --model_folder $fld --model_name "Lstm_128_32_2_1_5e4_256_35" --n_bins 50
 
-python ../output2histogram.py --model_folder $fld --model_name "Lstm_128_32_2_1_5e4_256_25" --n_bins 30
+python ../output2histogram.py --model_folder $fld --model_name "Lstm_128_32_2_1_5e4_256_25" --n_bins 50
 
-python ../output2histogram.py --model_folder $fld --model_name "Lstm_128_32_2_1_5e4_256_30" --n_bins 30
+python ../output2histogram.py --model_folder $fld --model_name "Lstm_128_32_2_1_5e4_256_30" --n_bins 50
 
-# python ../output2histogram.py --model_folder $fld --model_name "Lstm_128_32_1_1_5e4_256_35" --n_bins 30
+# python ../output2histogram.py --model_folder $fld --model_name "Lstm_128_32_1_1_5e4_256_35" --n_bins 50
