38d7be8d35b84a84cc6af74dc35316707ebde76d
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-12-23 17:52:27 -0300

    Fix

--- CNN/Inf_Seismic/exps/test1model.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p ../logs/
-
-# Test dataset inference on 2h5h5k_1e4_256_30 model
-echo "Running DAS test dataset inference on 1c1h_1e6_256_30 model"
-python ../inf_test_dataset.py --model_name 1c1h_1e6_256_30 --classifier 1c1h  > ../logs/1c1h_1e6_256_30.txt
-
-
