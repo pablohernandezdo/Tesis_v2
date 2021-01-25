3a5cd779e6658015a539f5bc1f5d90d8dff6595d
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-01-25 17:26:12 -0300

    LSTM models for final analysis

--- LSTM/exps/final_analysis_5_v2.sh
+++ LSTM/exps/final_analysis_5_v2.sh
@@ -3,4 +3,4 @@
 # Get best models PR, ROC and Fscore curves
 python ../final_analysis.py --step4_folder 'step4' \
                             --step5_folder 'step5' \
-                            --best_models "Lstm_16_16_1_1_5e3_256_35.txt Lstm_128_32_2_1_5e3_256_25.txt Lstm_64_64_5_1_1e2_256_25.txt Lstm_32_64_1_1_5e4_256_35.txt Lstm_16_16_1_1_5e3_256_35.txt"
+                            --best_models "Lstm_16_16_1_1_5e3_256_35.txt Lstm_64_64_5_1_1e2_256_25.txt Lstm_32_64_1_1_5e4_256_35.txt Lstm_128_32_2_1_5e3_256_25.txt Lstm_32_64_1_1_1e2_256_20.txt"
