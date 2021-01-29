6130cdeb9d8072faa311cb8ca4fef6007bd8b12e
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-01-28 22:18:10 -0300

    Create new datasets

--- Build-sets/exact.sh
+++ Build-sets/exact.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Building train, test and validation datasets"
-python createh5_v2.py --train_traces 5120 --train_noise 5120 \
-                      --test_traces 2048 --test_noise 2048   \
-                      --val_traces 2048 --val_noise 2048
+python createh5_v2.py --train_traces 38400 --train_noise 38400 \
+                      --test_traces 5120 --test_noise 5120   \
+                      --val_traces 5120 --val_noise 5120
