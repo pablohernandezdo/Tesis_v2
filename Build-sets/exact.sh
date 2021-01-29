7b147334789c1213956466726bfdd61351a7c609
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-01-28 22:40:08 -0300

    BIgger test and validation

--- Build-sets/exact.sh
+++ Build-sets/exact.sh
@@ -2,5 +2,5 @@
 
 echo "Building train, test and validation datasets"
 python createh5_v2.py --train_traces 38400 --train_noise 38400 \
-                      --test_traces 5120 --test_noise 5120   \
-                      --val_traces 5120 --val_noise 5120
+                      --test_traces 7680 --test_noise 7680   \
+                      --val_traces 7680 --val_noise 7680
