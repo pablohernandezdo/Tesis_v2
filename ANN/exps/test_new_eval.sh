eae4087832c64931f9b1dd4da643105798354d7c
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-02-21 14:18:47 -0300

    change model

--- ANN/exps/test_new_eval.sh
+++ ANN/exps/test_new_eval.sh
@@ -7,9 +7,9 @@ mkdir -p ../models/step4
 tst="Test_data.hdf5"
 trn="Train_data.hdf5"
 
-echo "Evaluating model 2h5h5k_1e3_256"
+echo "Evaluating model 2h5h4k_5e5_256_35"
 python ../eval.py \
               --test_path $tst \
               --train_path $trn \
               --model_folder step4 \
-              --classifier 2h5h5k --model_name 2h5h5k_1e3_256_20 > ../Analysis/logs/eval/step4/2h5h5k_1e3_256_20.txt
+              --classifier 2h5h4k --model_name 2h5h4k_5e5_256_35 > ../Analysis/logs/eval/step4/2h5h4k_5e5_256_35.txt
