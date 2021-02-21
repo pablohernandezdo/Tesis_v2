7615857f49556700235d168717354074a8c97b1f
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-02-21 19:35:16 -0300

    test train_val

--- ANN/exps/test_new_train.sh
+++ ANN/exps/test_new_train.sh
@@ -10,8 +10,13 @@ val="Validation_data.hdf5"
 
 # Learning rate 1e-3
 echo "Training model 2h5h5k, lr = 1e-3, epochs = 5, batch_size = 256"
-python ../train.py \
-              --model_folder step4 \
-              --train_path $trn \
-              --epochs 5 --lr 1e-3 --batch_size 256 \
-              --classifier 2h5h5k --model_name 2h5h5k_1e3_256_20 > ../Analysis/logs/train/step4/2h5h5k_1e3_256_20.txt
+python ../train_validation.py \
+              --train_path $trn     \
+              --val_path $val       \
+              --model_folder step4  \
+              --n_epochs 5          \
+              --lr 1e-3             \
+              --batch_size 256      \
+              --patience 20         \
+              --classifier 2h5h5k   \
+              --model_name 2h5h5k_1e3_256_20 > ../Analysis/logs/train/step4/2h5h5k_1e3_256_20.txt
