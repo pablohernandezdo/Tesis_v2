d3fdee4422b18b96e38add84668c47a6a4fc4d53
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-02-21 23:53:38 -0300

    a morir

--- ANN/figures_and_excel.py
+++ ANN/figures_and_excel.py
@@ -87,8 +87,11 @@ def main():
         # Obtain PR and ROC auc
         pr_auc, roc_auc = get_pr_roc_auc(prec, rec, fpr)
 
+        print(prec)
         print(rec)
 
+        break
+        
         # Get best threshold
         best_idx = np.argmax(fscore)
 
