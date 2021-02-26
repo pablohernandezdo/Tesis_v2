5783ce416b625d502a198d84262d6d083a3f7f71
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-02-25 22:40:55 -0300

    Fixx

--- Build-sets/create_geo.sh
+++ Build-sets/create_geo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Building train, test and validation datasets"
-python createh5_v2.py --train_traces 1 --train_noise 1 \
-                      --test_traces 1 --test_noise 1   \
-                      --val_traces 1 --val_noise 1
+python new_createh5.py --train_traces 1 --train_noise 1 \
+                       --test_traces 1 --test_noise 1   \
+                       --val_traces 1 --val_noise 1
 
 #echo "Building train, test and validation datasets"
 #python createh5_v2.py --train_traces 102400 --train_noise 51200 \
