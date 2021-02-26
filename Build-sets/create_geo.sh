e063d4d120f85f313fb951906e6d4fda68f9fb36
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-02-26 00:19:34 -0300

    mandarle con too

--- Build-sets/create_geo.sh
+++ Build-sets/create_geo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Building train, test and validation datasets"
-#python new_createh5.py --train_traces 102400 --train_noise 51200 \
-#                       --test_traces 12800 --test_noise 6400   \
-#                       --val_traces 12800 --val_noise 6400
+python new_createh5.py --train_traces 102400 --train_noise 51200 \
+                       --test_traces 12800 --test_noise 6400   \
+                       --val_traces 12800 --val_noise 6400
 
-python new_createh5.py --train_traces 1 --train_noise 1 \
-                       --test_traces 1 --test_noise 1   \
-                       --val_traces 1 --val_noise 1
-
+#python new_createh5.py --train_traces 1 --train_noise 1 \
+#                       --test_traces 1 --test_noise 1   \
+#                       --val_traces 1 --val_noise 1
+#
