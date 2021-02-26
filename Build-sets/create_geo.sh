efda3bd838b91043d7b55941e217541a1afb82ea
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-02-25 22:31:40 -0300

    ??

--- Build-sets/create_geo.sh
+++ Build-sets/create_geo.sh
@@ -4,7 +4,7 @@ echo "Building train, test and validation datasets"
 python createh5_v2.py --train_traces 1 --train_noise 1 \
                       --test_traces 1 --test_noise 1   \
                       --val_traces 1 --val_noise 1
-                      g
+
 #echo "Building train, test and validation datasets"
 #python createh5_v2.py --train_traces 102400 --train_noise 51200 \
 #                      --test_traces 12800 --test_noise 6400   \
