c6a4801a954bac879550827e9c61f99e41959910
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-03-11 21:11:24 -0300

    update checksets

--- Build-sets/checksets.sh
+++ Build-sets/checksets.sh
@@ -6,12 +6,20 @@ tst="Test_data.hdf5"
 
 # Check train dataset
 echo "Checking $trn dataset"
-python checkdataset.py --dataset_path '../Data/STEAD/Train_data.hdf5'
+python checkdataset.py --dataset_path '../Data/STEAD/Train_data_v3.hdf5'
 
 # Check validation dataset
 echo "Checking $val dataset"
-python checkdataset.py --dataset_path '../Data/STEAD/Validation_data.hdf5'
+python checkdataset.py --dataset_path '../Data/STEAD/Validation_data_v3.hdf5'
 
 # Check test dataset
 echo "Checking $tst dataset"
-python checkdataset.py --dataset_path '../Data/STEAD/Test_data.hdf5'
+python checkdataset.py --dataset_path '../Data/STEAD/STEAD_Seis_TEST.hdf5'
+
+# Check test dataset
+echo "Checking $tst dataset"
+python checkdataset.py --dataset_path '../Data/STEAD/STEAD_NSeis_TEST.hdf5'
+
+# Check test dataset
+echo "Checking $tst dataset"
+python checkdataset.py --dataset_path '../Data/STEAD/GEO_TEST.hdf5'
