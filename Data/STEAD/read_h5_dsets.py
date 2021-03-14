c1b178b5264ae7278316f93efee021a561750029
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-03-14 01:46:58 -0300

    ahora zi

--- Data/STEAD/read_h5_dsets.py
+++ Data/STEAD/read_h5_dsets.py
@@ -6,7 +6,7 @@ def main():
     with h5py.File('Train_data.hdf5', 'r') as h5_file:
         print(list(h5_file.keys()))
 
-    with h5py.File('test.hdf5', 'r') as h5_file:
+    with h5py.File('Test_geo.hdf5', 'r') as h5_file:
         print(list(h5_file.keys()))
 
 
