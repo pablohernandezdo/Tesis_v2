5751b5dda54357e97e00eb3cba0bde007ecebb51
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-03-14 01:48:07 -0300

    ?

--- Data/STEAD/read_h5_dsets.py
+++ Data/STEAD/read_h5_dsets.py
@@ -4,10 +4,12 @@ import h5py
 def main():
 
     with h5py.File('Train_data.hdf5', 'r') as h5_file:
-        print(list(h5_file.keys()))
-
+        for grp in list(h5_file.keys()):
+            print(list(h5_file[grp].keys()))
+        
     with h5py.File('Test_geo.hdf5', 'r') as h5_file:
-        print(list(h5_file.keys()))
+        for grp in list(h5_file.keys()):
+            print(list(h5_file[grp].keys()))
 
 
 if __name__ == "__main__":
