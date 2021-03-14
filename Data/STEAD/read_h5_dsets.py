ef105e3e1a621a1955ded533dd987e57916c8300
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-03-14 01:45:27 -0300

    watafak men

---
+++ Data/STEAD/read_h5_dsets.py
@@ -0,0 +1,15 @@
+import h5py
+
+
+def main():
+
+    with h5py.File('Train_data.hdf5', 'r') as h5_file:
+        print(list(h5_file.keys()))
+
+    with h5py.File('test.hdf5', 'r') as h5_file:
+        print(list(h5_file.keys()))
+
+
+if __name__ == "__main__":
+    main()
+
