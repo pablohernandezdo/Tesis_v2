37e29c8e712d90228b2b658c64106a64a4885971
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-03-20 18:12:19 -0300

    print more

--- Data/Belgica/qbp.py
+++ Data/Belgica/qbp.py
@@ -1,5 +1,8 @@
 import numpy as np
 import scipy.io as sio
+import matplotlib.pyplot as plt
+
+from scipy import signal
 
 
 def main():
@@ -10,6 +13,12 @@ def main():
     fs = 10
     n = traces.shape[1]
     t_ax = np.arange(n) / fs
+
+    # avg_trace = np.mean(traces[3500:4001, :], 0)
+    # avg_trace = signal.resample(avg_trace, n * 10)
+    #
+    # plt.plot(avg_trace)
+    # plt.show()
 
     qbps = []
 
@@ -23,7 +32,7 @@ def main():
 
     # sorted_qbps = [qbps[i] for i in sorted_idxs[::-1]]
 
-    print(sorted_idxs[:20])
+    print(sorted_idxs[:144])
 
 
 if __name__ == '__main__':
