cb278c196ec463006e2dcc41aed892ed50ac8491
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-03-21 21:55:26 -0300

    eval das_sep

---
+++ CNN/eval_das_sep.py
@@ -0,0 +1,614 @@
+import time
+import argparse
+from pathlib import Path
+
+import tqdm
+import torch
+import pandas as pd
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from humanfriendly import format_timespan
+
+from model import *
+from dataset import HDF5Dataset
+
+
+def main():
+    # Measure exec time
+    start_time = time.time()
+
+    # Args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", default='1h6k_test_model',
+                        help="Name of model to eval")
+    parser.add_argument("--model_folder", default='test',
+                        help="Model to eval folder")
+    parser.add_argument("--classifier", default='1h6k',
+                        help="Choose classifier architecture")
+    parser.add_argument("--das_seis_path",
+                        default='DAS_seismic.hdf5',
+                        help="HDF5 DAS seimic dataset path")
+    parser.add_argument("--das_nseis_path",
+                        default='DAS_non_seismic.hdf5',
+                        help="HDF5 DAS non seimic dataset path")
+    parser.add_argument("--das_noise_path",
+                        default='DAS_noise.hdf5',
+                        help="HDF5 DAS noise dataset path")
+    parser.add_argument("--batch_size", type=int, default=256,
+                        help="Size of the training batches")
+    args = parser.parse_args()
+
+    # Select training device
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # DAS Seismic test dataset
+    test_set = HDF5Dataset(args.das_seis_path)
+    das_seis_loader = DataLoader(test_set,
+                                        batch_size=args.batch_size,
+                                        shuffle=True)
+
+    # DAS NSeismic test dataset
+    test_set = HDF5Dataset(args.das_nseis_path)
+    das_nseis_loader = DataLoader(test_set,
+                                         batch_size=args.batch_size,
+                                         shuffle=True)
+
+    # DAS noise test dataset
+    test_set = HDF5Dataset(args.das_noise_path)
+    das_noise_loader = DataLoader(test_set,
+                                 batch_size=args.batch_size, shuffle=True)
+
+    # Load specified Classifier
+    net = get_classifier(args.classifier)
+    net.to(device)
+
+    # Count number of parameters
+    params = count_parameters(net)
+
+    # Load from trained model
+    net.load_state_dict(torch.load(f'../models/{args.model_folder}/'
+                                   f'{args.model_name}.pth'))
+    net.eval()
+
+    # Evaluate model on STEAD seismic test set
+    evaluate_dataset(das_seis_loader, 'DAS_seismic', device,
+                     net, args.model_name, args.model_folder,
+                     '../Results/Testing/Outputs')
+
+    # Evaluate model on STEAD seismic test set
+    evaluate_dataset(das_nseis_loader, 'DAS_non_seismic', device,
+                     net, args.model_name, args.model_folder,
+                     '../Results/Testing/Outputs')
+
+    # Evaluate model on STEAD seismic test set
+    evaluate_dataset(das_noise_loader, 'DAS_noise', device,
+                     net, args.model_name, args.model_folder,
+                     '../Results/Testing/Outputs')
+
+    total_time = time.time() - start_time
+
+    print(f'Total execution time: {format_timespan(total_time)}\n'
+          f'Number of network parameters: {params}')
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def evaluate_dataset(data_loader, dataset_name, device, net,
+                     model_name, model_folder, csv_folder):
+
+    plt.figure()
+
+    i = 0
+    j = 0
+    thr = 0.5
+
+    Path(f"../Results/Classified/{model_folder}/"
+         f"{model_name}_({thr})/Seis/").mkdir(parents=True, exist_ok=True)
+
+    Path(f"../Results/Classified/{model_folder}/"
+         f"{model_name}_({thr})/NSeis/").mkdir(parents=True, exist_ok=True)
+
+    # List of outputs and labels used to create pd dataframe
+    dataframe_rows_list = []
+
+    with tqdm.tqdm(total=len(data_loader),
+                   desc=f'{dataset_name} dataset evaluation') as data_bar:
+
+        with torch.no_grad():
+            for data in data_loader:
+
+                traces, labels = data[0].to(device), data[1].to(device)
+                outputs = net(traces)
+
+                for out, tr in zip(outputs, traces):
+                    if out.item() > thr and i < 500:
+                        plt.clf()
+                        plt.plot(tr.cpu())
+                        plt.xlabel('Samples')
+                        plt.ylabel('Amplitude')
+                        plt.title(f'{dataset_name}_{i}.png')
+                        plt.grid(True)
+                        plt.savefig(f'../Results/Classified/'
+                                    f'{model_folder}/{model_name}_({thr})/Seis/'
+                                    f'{dataset_name}_{i}_{out.item():5.3f}.png')
+
+                        i += 1
+
+                    elif out.item() < thr and j < 500:
+                        plt.clf()
+                        plt.plot(tr.cpu())
+                        plt.xlabel('Samples')
+                        plt.ylabel('Amplitude')
+                        plt.title(f'{dataset_name}_{j}.png')
+                        plt.grid(True)
+                        plt.savefig(f'../Results/Classified/'
+                                    f'{model_folder}/{model_name}_({thr})/'
+                                    f'NSeis/'
+                                    f'{dataset_name}_{j}_{out.item():5.3f}.png')
+
+                        j += 1
+
+                for out, lab in zip(outputs, labels):
+                    new_row = {'out': out.item(), 'label': lab.item()}
+                    dataframe_rows_list.append(new_row)
+
+                data_bar.update(1)
+
+    test_outputs = pd.DataFrame(dataframe_rows_list)
+
+    # Create csv folder if necessary
+    save_folder = f'{csv_folder}/{dataset_name}/{model_folder}'
+    Path(save_folder).mkdir(parents=True, exist_ok=True)
+
+    # Save outputs and labels to csv file
+    test_outputs.to_csv(f'{save_folder}/{model_name}.csv', index=False)
+
+
+def get_classifier(x):
+    if x == 'Cnn1_6k':
+        return Cnn1_6k()
+    if x == 'Cnn1_5k':
+        return Cnn1_5k()
+    if x == 'Cnn1_4k':
+        return Cnn1_4k()
+    if x == 'Cnn1_3k':
+        return Cnn1_3k()
+    if x == 'Cnn1_2k':
+        return Cnn1_2k()
+    if x == 'Cnn1_1k':
+        return Cnn1_1k()
+    if x == 'Cnn1_5h':
+        return Cnn1_5h()
+    if x == 'Cnn1_2h':
+        return Cnn1_2h()
+    if x == 'Cnn1_5h':
+        return Cnn1_1h()
+    if x == 'Cnn1_10':
+        return Cnn1_10()
+    if x == 'Cnn1_6k_6k':
+        return Cnn1_6k_6k()
+    if x == 'Cnn1_6k_5k':
+        return Cnn1_6k_5k()
+    if x == 'Cnn1_6k_4k':
+        return Cnn1_6k_4k()
+    if x == 'Cnn1_6k_3k':
+        return Cnn1_6k_3k()
+    if x == 'Cnn1_6k_2k':
+        return Cnn1_6k_2k()
+    if x == 'Cnn1_6k_1k':
+        return Cnn1_6k_1k()
+    if x == 'Cnn1_6k_5h':
+        return Cnn1_6k_5h()
+    if x == 'Cnn1_6k_2h':
+        return Cnn1_6k_2h()
+    if x == 'Cnn1_6k_1h':
+        return Cnn1_6k_1h()
+    if x == 'Cnn1_6k_10':
+        return Cnn1_6k_10()
+    if x == 'Cnn1_5k_6k':
+        return Cnn1_5k_6k()
+    if x == 'Cnn1_5k_5k':
+        return Cnn1_5k_5k()
+    if x == 'Cnn1_5k_4k':
+        return Cnn1_5k_4k()
+    if x == 'Cnn1_5k_3k':
+        return Cnn1_5k_3k()
+    if x == 'Cnn1_5k_2k':
+        return Cnn1_5k_2k()
+    if x == 'Cnn1_5k_1k':
+        return Cnn1_5k_1k()
+    if x == 'Cnn1_5k_5h':
+        return Cnn1_5k_5h()
+    if x == 'Cnn1_5k_2h':
+        return Cnn1_5k_2h()
+    if x == 'Cnn1_5k_1h':
+        return Cnn1_5k_1h()
+    if x == 'Cnn1_5k_10':
+        return Cnn1_5k_10()
+    if x == 'Cnn1_4k_6k':
+        return Cnn1_4k_6k()
+    if x == 'Cnn1_4k_5k':
+        return Cnn1_4k_5k()
+    if x == 'Cnn1_4k_4k':
+        return Cnn1_4k_4k()
+    if x == 'Cnn1_4k_3k':
+        return Cnn1_4k_3k()
+    if x == 'Cnn1_4k_2k':
+        return Cnn1_4k_2k()
+    if x == 'Cnn1_4k_1k':
+        return Cnn1_4k_1k()
+    if x == 'Cnn1_4k_5h':
+        return Cnn1_4k_5h()
+    if x == 'Cnn1_4k_2h':
+        return Cnn1_4k_2h()
+    if x == 'Cnn1_4k_1h':
+        return Cnn1_4k_1h()
+    if x == 'Cnn1_4k_10':
+        return Cnn1_4k_10()
+    if x == 'Cnn1_3k_6k':
+        return Cnn1_3k_6k()
+    if x == 'Cnn1_3k_5k':
+        return Cnn1_3k_5k()
+    if x == 'Cnn1_3k_4k':
+        return Cnn1_3k_4k()
+    if x == 'Cnn1_3k_3k':
+        return Cnn1_3k_3k()
+    if x == 'Cnn1_3k_2k':
+        return Cnn1_3k_2k()
+    if x == 'Cnn1_3k_1k':
+        return Cnn1_3k_1k()
+    if x == 'Cnn1_3k_5h':
+        return Cnn1_3k_5h()
+    if x == 'Cnn1_3k_2h':
+        return Cnn1_3k_2h()
+    if x == 'Cnn1_3k_1h':
+        return Cnn1_3k_1h()
+    if x == 'Cnn1_3k_10':
+        return Cnn1_3k_10()
+    if x == 'Cnn1_2k_6k':
+        return Cnn1_2k_6k()
+    if x == 'Cnn1_2k_5k':
+        return Cnn1_2k_5k()
+    if x == 'Cnn1_2k_4k':
+        return Cnn1_2k_4k()
+    if x == 'Cnn1_2k_3k':
+        return Cnn1_2k_3k()
+    if x == 'Cnn1_2k_2k':
+        return Cnn1_2k_2k()
+    if x == 'Cnn1_2k_1k':
+        return Cnn1_2k_1k()
+    if x == 'Cnn1_2k_5h':
+        return Cnn1_2k_5h()
+    if x == 'Cnn1_2k_2h':
+        return Cnn1_2k_2h()
+    if x == 'Cnn1_2k_1h':
+        return Cnn1_2k_1h()
+    if x == 'Cnn1_2k_10':
+        return Cnn1_2k_10()
+    if x == 'Cnn1_1k_6k':
+        return Cnn1_1k_6k()
+    if x == 'Cnn1_1k_5k':
+        return Cnn1_1k_5k()
+    if x == 'Cnn1_1k_4k':
+        return Cnn1_1k_4k()
+    if x == 'Cnn1_1k_3k':
+        return Cnn1_1k_3k()
+    if x == 'Cnn1_1k_2k':
+        return Cnn1_1k_2k()
+    if x == 'Cnn1_1k_1k':
+        return Cnn1_1k_1k()
+    if x == 'Cnn1_1k_5h':
+        return Cnn1_1k_5h()
+    if x == 'Cnn1_1k_2h':
+        return Cnn1_1k_2h()
+    if x == 'Cnn1_1k_1h':
+        return Cnn1_1k_1h()
+    if x == 'Cnn1_1k_10':
+        return Cnn1_1k_10()
+    if x == 'Cnn1_5h_6k':
+        return Cnn1_5h_6k()
+    if x == 'Cnn1_5h_5k':
+        return Cnn1_5h_5k()
+    if x == 'Cnn1_5h_4k':
+        return Cnn1_5h_4k()
+    if x == 'Cnn1_5h_3k':
+        return Cnn1_5h_3k()
+    if x == 'Cnn1_5h_2k':
+        return Cnn1_5h_2k()
+    if x == 'Cnn1_5h_1k':
+        return Cnn1_5h_1k()
+    if x == 'Cnn1_5h_5h':
+        return Cnn1_5h_5h()
+    if x == 'Cnn1_5h_2h':
+        return Cnn1_5h_2h()
+    if x == 'Cnn1_5h_1h':
+        return Cnn1_5h_1h()
+    if x == 'Cnn1_5h_10':
+        return Cnn1_5h_10()
+    if x == 'Cnn1_2h_6k':
+        return Cnn1_2h_6k()
+    if x == 'Cnn1_2h_5k':
+        return Cnn1_2h_5k()
+    if x == 'Cnn1_2h_4k':
+        return Cnn1_2h_4k()
+    if x == 'Cnn1_2h_3k':
+        return Cnn1_2h_3k()
+    if x == 'Cnn1_2h_2k':
+        return Cnn1_2h_2k()
+    if x == 'Cnn1_2h_1k':
+        return Cnn1_2h_1k()
+    if x == 'Cnn1_2h_5h':
+        return Cnn1_2h_5h()
+    if x == 'Cnn1_2h_2h':
+        return Cnn1_2h_2h()
+    if x == 'Cnn1_2h_1h':
+        return Cnn1_2h_1h()
+    if x == 'Cnn1_2h_10':
+        return Cnn1_2h_10()
+    if x == 'Cnn1_1h_6k':
+        return Cnn1_1h_6k()
+    if x == 'Cnn1_1h_5k':
+        return Cnn1_1h_5k()
+    if x == 'Cnn1_1h_4k':
+        return Cnn1_1h_4k()
+    if x == 'Cnn1_1h_3k':
+        return Cnn1_1h_3k()
+    if x == 'Cnn1_1h_2k':
+        return Cnn1_1h_2k()
+    if x == 'Cnn1_1h_1k':
+        return Cnn1_1h_1k()
+    if x == 'Cnn1_1h_5h':
+        return Cnn1_1h_5h()
+    if x == 'Cnn1_1h_2h':
+        return Cnn1_1h_2h()
+    if x == 'Cnn1_1h_1h':
+        return Cnn1_1h_1h()
+    if x == 'Cnn1_1h_10':
+        return Cnn1_1h_10()
+    if x == 'Cnn1_10_6k':
+        return Cnn1_10_6k()
+    if x == 'Cnn1_10_5k':
+        return Cnn1_10_5k()
+    if x == 'Cnn1_10_4k':
+        return Cnn1_10_4k()
+    if x == 'Cnn1_10_3k':
+        return Cnn1_10_3k()
+    if x == 'Cnn1_10_2k':
+        return Cnn1_10_2k()
+    if x == 'Cnn1_10_1k':
+        return Cnn1_10_1k()
+    if x == 'Cnn1_10_5h':
+        return Cnn1_10_5h()
+    if x == 'Cnn1_10_2h':
+        return Cnn1_10_2h()
+    if x == 'Cnn1_10_1h':
+        return Cnn1_10_1h()
+    if x == 'Cnn1_10_10':
+        return Cnn1_10_10()
+    if x == 'Cnn2_6k':
+        return Cnn2_6k()
+    if x == 'Cnn2_5k':
+        return Cnn2_5k()
+    if x == 'Cnn2_4k':
+        return Cnn2_4k()
+    if x == 'Cnn2_3k':
+        return Cnn2_3k()
+    if x == 'Cnn2_2k':
+        return Cnn2_2k()
+    if x == 'Cnn2_1k':
+        return Cnn2_1k()
+    if x == 'Cnn2_5h':
+        return Cnn2_5h()
+    if x == 'Cnn2_2h':
+        return Cnn2_2h()
+    if x == 'Cnn2_5h':
+        return Cnn2_1h()
+    if x == 'Cnn2_10':
+        return Cnn2_10()
+    if x == 'Cnn2_6k_6k':
+        return Cnn2_6k_6k()
+    if x == 'Cnn2_6k_5k':
+        return Cnn2_6k_5k()
+    if x == 'Cnn2_6k_4k':
+        return Cnn2_6k_4k()
+    if x == 'Cnn2_6k_3k':
+        return Cnn2_6k_3k()
+    if x == 'Cnn2_6k_2k':
+        return Cnn2_6k_2k()
+    if x == 'Cnn2_6k_1k':
+        return Cnn2_6k_1k()
+    if x == 'Cnn2_6k_5h':
+        return Cnn2_6k_5h()
+    if x == 'Cnn2_6k_2h':
+        return Cnn2_6k_2h()
+    if x == 'Cnn2_6k_1h':
+        return Cnn2_6k_1h()
+    if x == 'Cnn2_6k_10':
+        return Cnn2_6k_10()
+    if x == 'Cnn2_5k_6k':
+        return Cnn2_5k_6k()
+    if x == 'Cnn2_5k_5k':
+        return Cnn2_5k_5k()
+    if x == 'Cnn2_5k_4k':
+        return Cnn2_5k_4k()
+    if x == 'Cnn2_5k_3k':
+        return Cnn2_5k_3k()
+    if x == 'Cnn2_5k_2k':
+        return Cnn2_5k_2k()
+    if x == 'Cnn2_5k_1k':
+        return Cnn2_5k_1k()
+    if x == 'Cnn2_5k_5h':
+        return Cnn2_5k_5h()
+    if x == 'Cnn2_5k_2h':
+        return Cnn2_5k_2h()
+    if x == 'Cnn2_5k_1h':
+        return Cnn2_5k_1h()
+    if x == 'Cnn2_5k_10':
+        return Cnn2_5k_10()
+    if x == 'Cnn2_4k_6k':
+        return Cnn2_4k_6k()
+    if x == 'Cnn2_4k_5k':
+        return Cnn2_4k_5k()
+    if x == 'Cnn2_4k_4k':
+        return Cnn2_4k_4k()
+    if x == 'Cnn2_4k_3k':
+        return Cnn2_4k_3k()
+    if x == 'Cnn2_4k_2k':
+        return Cnn2_4k_2k()
+    if x == 'Cnn2_4k_1k':
+        return Cnn2_4k_1k()
+    if x == 'Cnn2_4k_5h':
+        return Cnn2_4k_5h()
+    if x == 'Cnn2_4k_2h':
+        return Cnn2_4k_2h()
+    if x == 'Cnn2_4k_1h':
+        return Cnn2_4k_1h()
+    if x == 'Cnn2_4k_10':
+        return Cnn2_4k_10()
+    if x == 'Cnn2_3k_6k':
+        return Cnn2_3k_6k()
+    if x == 'Cnn2_3k_5k':
+        return Cnn2_3k_5k()
+    if x == 'Cnn2_3k_4k':
+        return Cnn2_3k_4k()
+    if x == 'Cnn2_3k_3k':
+        return Cnn2_3k_3k()
+    if x == 'Cnn2_3k_2k':
+        return Cnn2_3k_2k()
+    if x == 'Cnn2_3k_1k':
+        return Cnn2_3k_1k()
+    if x == 'Cnn2_3k_5h':
+        return Cnn2_3k_5h()
+    if x == 'Cnn2_3k_2h':
+        return Cnn2_3k_2h()
+    if x == 'Cnn2_3k_1h':
+        return Cnn2_3k_1h()
+    if x == 'Cnn2_3k_10':
+        return Cnn2_3k_10()
+    if x == 'Cnn2_2k_6k':
+        return Cnn2_2k_6k()
+    if x == 'Cnn2_2k_5k':
+        return Cnn2_2k_5k()
+    if x == 'Cnn2_2k_4k':
+        return Cnn2_2k_4k()
+    if x == 'Cnn2_2k_3k':
+        return Cnn2_2k_3k()
+    if x == 'Cnn2_2k_2k':
+        return Cnn2_2k_2k()
+    if x == 'Cnn2_2k_1k':
+        return Cnn2_2k_1k()
+    if x == 'Cnn2_2k_5h':
+        return Cnn2_2k_5h()
+    if x == 'Cnn2_2k_2h':
+        return Cnn2_2k_2h()
+    if x == 'Cnn2_2k_1h':
+        return Cnn2_2k_1h()
+    if x == 'Cnn2_2k_10':
+        return Cnn2_2k_10()
+    if x == 'Cnn2_1k_6k':
+        return Cnn2_1k_6k()
+    if x == 'Cnn2_1k_5k':
+        return Cnn2_1k_5k()
+    if x == 'Cnn2_1k_4k':
+        return Cnn2_1k_4k()
+    if x == 'Cnn2_1k_3k':
+        return Cnn2_1k_3k()
+    if x == 'Cnn2_1k_2k':
+        return Cnn2_1k_2k()
+    if x == 'Cnn2_1k_1k':
+        return Cnn2_1k_1k()
+    if x == 'Cnn2_1k_5h':
+        return Cnn2_1k_5h()
+    if x == 'Cnn2_1k_2h':
+        return Cnn2_1k_2h()
+    if x == 'Cnn2_1k_1h':
+        return Cnn2_1k_1h()
+    if x == 'Cnn2_1k_10':
+        return Cnn2_1k_10()
+    if x == 'Cnn2_5h_6k':
+        return Cnn2_5h_6k()
+    if x == 'Cnn2_5h_5k':
+        return Cnn2_5h_5k()
+    if x == 'Cnn2_5h_4k':
+        return Cnn2_5h_4k()
+    if x == 'Cnn2_5h_3k':
+        return Cnn2_5h_3k()
+    if x == 'Cnn2_5h_2k':
+        return Cnn2_5h_2k()
+    if x == 'Cnn2_5h_1k':
+        return Cnn2_5h_1k()
+    if x == 'Cnn2_5h_5h':
+        return Cnn2_5h_5h()
+    if x == 'Cnn2_5h_2h':
+        return Cnn2_5h_2h()
+    if x == 'Cnn2_5h_1h':
+        return Cnn2_5h_1h()
+    if x == 'Cnn2_5h_10':
+        return Cnn2_5h_10()
+    if x == 'Cnn2_2h_6k':
+        return Cnn2_2h_6k()
+    if x == 'Cnn2_2h_5k':
+        return Cnn2_2h_5k()
+    if x == 'Cnn2_2h_4k':
+        return Cnn2_2h_4k()
+    if x == 'Cnn2_2h_3k':
+        return Cnn2_2h_3k()
+    if x == 'Cnn2_2h_2k':
+        return Cnn2_2h_2k()
+    if x == 'Cnn2_2h_1k':
+        return Cnn2_2h_1k()
+    if x == 'Cnn2_2h_5h':
+        return Cnn2_2h_5h()
+    if x == 'Cnn2_2h_2h':
+        return Cnn2_2h_2h()
+    if x == 'Cnn2_2h_1h':
+        return Cnn2_2h_1h()
+    if x == 'Cnn2_2h_10':
+        return Cnn2_2h_10()
+    if x == 'Cnn2_1h_6k':
+        return Cnn2_1h_6k()
+    if x == 'Cnn2_1h_5k':
+        return Cnn2_1h_5k()
+    if x == 'Cnn2_1h_4k':
+        return Cnn2_1h_4k()
+    if x == 'Cnn2_1h_3k':
+        return Cnn2_1h_3k()
+    if x == 'Cnn2_1h_2k':
+        return Cnn2_1h_2k()
+    if x == 'Cnn2_1h_1k':
+        return Cnn2_1h_1k()
+    if x == 'Cnn2_1h_5h':
+        return Cnn2_1h_5h()
+    if x == 'Cnn2_1h_2h':
+        return Cnn2_1h_2h()
+    if x == 'Cnn2_1h_1h':
+        return Cnn2_1h_1h()
+    if x == 'Cnn2_1h_10':
+        return Cnn2_1h_10()
+    if x == 'Cnn2_10_6k':
+        return Cnn2_10_6k()
+    if x == 'Cnn2_10_5k':
+        return Cnn2_10_5k()
+    if x == 'Cnn2_10_4k':
+        return Cnn2_10_4k()
+    if x == 'Cnn2_10_3k':
+        return Cnn2_10_3k()
+    if x == 'Cnn2_10_2k':
+        return Cnn2_10_2k()
+    if x == 'Cnn2_10_1k':
+        return Cnn2_10_1k()
+    if x == 'Cnn2_10_5h':
+        return Cnn2_10_5h()
+    if x == 'Cnn2_10_2h':
+        return Cnn2_10_2h()
+    if x == 'Cnn2_10_1h':
+        return Cnn2_10_1h()
+    if x == 'Cnn2_10_10':
+        return Cnn2_10_10()
+    else:
+        return Cnn2_10_10()
+
+if __name__ == "__main__":
+    main()
