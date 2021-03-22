c7d24ca7be4bf0c8e724cfbdb960d0918f15f929
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-03-21 21:58:15 -0300

    das sep exp

---
+++ CNN/exps/eval_das_sep.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p ../Results/Testing/Logs/DAS_sep
+
+das_seis="DAS_seismic.hdf5"
+das_nseis="DAS_non_seismic.hdf5"
+das_noise="DAS_noise.hdf5"
+
+echo "Evaluating model Cnn1_3k_10_1e4_256"
+python ../eval_das.py \
+              --das_seis_path $das_seis \
+              --das_nseis_path $das_nseis \
+              --das_noise_path $das_noise \
+              --classifier Cnn1_3k_10 \
+              --model_folder separated \
+              --model_name Cnn1_3k_10_1e4_256_40 > ../Results/Testing/Logs/DAS_sep/Cnn1_3k_10_1e4_256_40.txt
