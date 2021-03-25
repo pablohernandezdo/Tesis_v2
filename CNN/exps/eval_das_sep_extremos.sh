fbde6dfbf57ab377f56529184ac25ac3408dd8de
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2021-03-25 15:19:50 -0300

    aohra si

--- CNN/exps/eval_das_sep_extremos.sh
+++ CNN/exps/eval_das_sep_extremos.sh
@@ -7,11 +7,10 @@ das_nseis="DAS_non_seismic.hdf5"
 das_noise="DAS_noise.hdf5"
 
 echo "Evaluating model Cnn1_3k_10_1e4_256"
-python ../eval_das_sep.py \
+python ../eval_das_sep_extremos.py \
               --das_seis_path $das_seis \
               --das_nseis_path $das_nseis \
               --das_noise_path $das_noise \
               --classifier Cnn1_3k_10 \
               --model_folder separated \
               --model_name Cnn1_3k_10_1e4_256_40 > ../Results/Testing/Logs/DAS_sep/Cnn1_3k_10_1e4_256_40.txt
-g
